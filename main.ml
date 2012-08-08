b1b65006025d30b9f921eade2bac2c7b83af2aee
Simon Cruanes <simon.cruanes.2007@m4x.org>
2012-08-08 09:42:03 +0200

    main.ml for parsing and pretty printing the given file

--- main.ml
+++ main.ml
@@ -1,3 +1,27 @@
 (* main file *)
 
-let () = ()
+
+(* get first file of command line arguments *)
+let get_file () =
+  let files = ref [] in
+  Arg.parse [] (fun s -> files := s :: !files) "./prover file";
+  match !files with
+  | [] -> failwith "file required."
+  | (x::_) -> x
+
+
+(* parse given tptp file *)
+let parse_file f =
+  let input = open_in f in
+  try
+    let buf = Lexing.from_channel input in
+    Parser_tptp.parse_file Lexer_tptp.token buf
+  with _ as e -> close_in input; raise e
+
+module Pretty = Pp.Pp(Terms.Default)
+
+let () =
+  let f = get_file () in
+  Printf.printf "process file %s\n" f;
+  let clauses, _ = parse_file f in
+  Format.printf "@[<hv 2>%a@]" (Pretty.pp_list ~sep:"," Pretty.pp_clause) clauses
