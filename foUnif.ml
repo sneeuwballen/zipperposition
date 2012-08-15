3a347ab2c3be73aabb8f0dfe7bff7a90be45eba5
simon <simon.cruanes.2007@m4x.org>
2012-08-15 13:14:54 +0200

    bugfix in the unification algorithm

--- foUnif.ml
+++ foUnif.ml
@@ -13,16 +13,17 @@ open Types
 open Hashcons
 
 module T = Terms
-module Subst = FoSubst
+module S = FoSubst
 
 (* returns (a in l, b in l) *)
 let mem2 a b l =
   let rec aux found_a found_b = function
     | x :: tl ->
-       let found_a = found_a || x = a in
-       let found_b = found_b || x = b in
-       if found_a && found_b then true, true
-       else aux found_a found_b tl
+      let found_a = found_a || x = a in
+      let found_b = found_b || x = b in
+      if found_a && found_b
+        then true, true
+        else aux found_a found_b tl
     | [] -> found_a, found_b
   in
    aux false false l
@@ -30,8 +31,8 @@ let mem2 a b l =
 (* do both unification and match *)
 let rec unif locked_vars subst s t =
   if s.node.sort <> t.node.sort then raise (UnificationFailure (lazy "different sorts"));
-  let s = match s.node.term with Var _ -> Subst.lookup s subst | _ -> s
-  and t = match t.node.term with Var _ -> Subst.lookup t subst | _ -> t in
+  let s = match s.node.term with Var _ -> S.apply_subst subst s | _ -> s
+  and t = match t.node.term with Var _ -> S.apply_subst subst t | _ -> t in
   match s.node.term, t.node.term with
   | _, _ when T.eq_foterm s t -> subst
   | Var _, Var _ ->
@@ -40,15 +41,15 @@ let rec unif locked_vars subst s t =
         if t_locked then
           raise (UnificationFailure (lazy "Inference.unification.unif"))
         else
-          Subst.build_subst t s subst
+          S.build_subst t s subst
       else
-        Subst.build_subst s t subst
+        S.build_subst s t subst
   | Var _, _ when occurs_check subst s t || List.mem s locked_vars ->
       raise (UnificationFailure (lazy "Inference.unification.unif"))
-  | Var _, _ -> Subst.build_subst s t subst
+  | Var _, _ -> S.build_subst s t subst
   | _, Var _ when occurs_check subst t s || List.mem t locked_vars ->
       raise (UnificationFailure (lazy "Inference.unification.unif"))
-  | _, Var _ -> Subst.build_subst t s subst
+  | _, Var _ -> S.build_subst t s subst
   | Node l1, Node l2 -> (
       try
         List.fold_left2
@@ -63,30 +64,31 @@ and occurs_check subst what where =
   match where.node.term with
   | Var _ when T.eq_foterm where what -> true
   | Var _ ->
-      let t = Subst.lookup where subst in
+      let t = S.lookup where subst in
       if not (T.eq_foterm t where)
-        then occurs_check subst what t else false
+        then occurs_check subst what t
+        else false
   | Node l -> List.exists (occurs_check subst what) l
   | _ -> false
 
 (* full unification, no locked variables *)
-let unification a b = unif [] Subst.id_subst a b
+let unification a b = unif [] S.id_subst a b
 
 (* matching, lock variables of b (b must generalize a) *)
-let matching a b = unif (T.vars_of_term b) Subst.id_subst a b
+let matching a b = unif (T.vars_of_term b) S.id_subst a b
 
 (* Sets of variables in s and t are assumed to be disjoint  *)
 let alpha_eq s t =
   let rec equiv subst s t =
-    let s = match s.node.term with Var _ -> Subst.lookup s subst | _ -> s
-    and t = match t.node.term with Var _ -> Subst.lookup t subst | _ -> t
+    let s = match s.node.term with Var _ -> S.lookup s subst | _ -> s
+    and t = match t.node.term with Var _ -> S.lookup t subst | _ -> t
 
     in
     match s.node.term, t.node.term with
       | _, _ when T.eq_foterm s t -> subst
       | Var _, Var _
           when (not (List.exists (fun (_,k) -> k=t) subst)) ->
-          let subst = Subst.build_subst s t subst in
+          let subst = S.build_subst s t subst in
             subst
       | Node l1, Node l2 -> (
           try
@@ -99,5 +101,5 @@ let alpha_eq s t =
       | _, _ ->
           raise (UnificationFailure (lazy "Inference.unification.unif"))
   in
-    equiv Subst.id_subst s t
+    equiv S.id_subst s t
 
