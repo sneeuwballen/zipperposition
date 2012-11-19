34c9e5a119d1d4ded68020d380e058717b8b9a44
Simon Cruanes <simon.cruanes.2007@m4x.org>
2012-11-19 11:39:01 +0100

    first (somehow) working version of fast discrimination trees

--- src/dtree.ml
+++ src/dtree.ml
@@ -19,8 +19,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 *)
 
 open Types
+
 module T = Terms
 module S = FoSubst
+module Utils = FoUtils
 
 (** Efficient perfect discrimination trees for matching *)
 
@@ -31,15 +33,16 @@ module S = FoSubst
 (** index of subterm in prefix traversal *)
 type position = int
 
-(** get subterm by its position *)(
+(** get subterm by its position *)
 let rec get_pos t pos = 
   match t.term, pos with
   | _, 0 -> t
-  | Node (_::l) -> get_subpos l pos
+  | Node (hd::l), _ -> get_subpos l (pos - hd.tsize)
+  | _ -> assert false
 and get_subpos l pos =
   match l, pos with
-  | t::l', _ when t.tsize <= pos -> get_subpos l' (pos - t.tsize)
-  | t::l', _ -> get_pos t pos  (* search inside the term *)
+  | t::l', _ when t.tsize > pos -> get_pos t pos  (* search inside the term *)
+  | t::l', _ -> get_subpos l' (pos - t.tsize) (* continue to next term *)
   | [], _ -> assert false
 
 (** get position of next term *)
@@ -47,39 +50,18 @@ let next t pos = pos+1
 
 (** skip subterms, got to next term that is not a subterm of t|pos *)
 let skip t pos =
-  let t = get_pos t pos in
-  pos + t.tsize
-
-(** arity of the subterm of t which is at given pos *)
-let arity t pos =
-  match get_pos t pos with
-  | Leaf _ | Var _ -> 0
-  | Node (_::l) -> List.length l
-  | Node [] -> assert false
+  let t_pos = get_pos t pos in
+  pos + t_pos.tsize
 
 (** maximum position in the term *)
 let maxpos t = t.tsize - 1
 
-type character =
-  | CVar of sort * int * term           (** var index, var *)
-  | CSymb of sort * int * string        (** symbol, arity *)
-
-let eq_character c1 c2 =
-  match c1, c2 with
-  | CVar (_, _, t1), CVar (_, _, t2) -> t1 == t2
-  | CSymb (s1, i1, f1), CSymb (s2, i2, f2) ->
-    s1 = s2 && i1 = i2 && f1 = f2
-  | _ -> false
-
-(** convert a term into a character (the first character of the term) *)
+(** find first atomic term of t *)
 let rec term_to_char t =
   match t.term with
-  | Var i -> CVar (t.sort, i, t)
-  | Leaf s -> CSymb (t.sort, arity t 0, s)
-  | Node (hd::_) ->  (* recurse to get the symbol *)
-    (match term_to_char hd with
-    | CVar _ -> assert false
-    | CSymb (_, _, s) -> (t.sort, arity t 0, s))
+  | Var _ | Leaf _ -> t
+  | Node (hd::_) -> term_to_char hd (* recurse to get the symbol *)
+  | Node [] -> assert false
 
 (** convert term to list of char *)
 let to_list t =
@@ -97,59 +79,50 @@ let to_list t =
  * -------------------------------------------------------- *)
 
 type 'a trie =
-  | Node of (character * 'a trie) list  (** map char -> trie *)
+  | Node of 'a trie Terms.TMap.t        (** map atom -> trie *)
   | Leaf of (term * 'a * int) list      (** leaf with (term, value, priority) list *)
+
+let empty_trie n = match n with
+  | Node m when T.TMap.is_empty m -> true
+  | Leaf [] -> true
+  | _ -> false
 
 (** get/add/remove the leaf for the given flatterm. The
     continuation k takes the leaf, and returns a leaf option
     that replaces the old leaf. 
     This function returns the new trie. *)
-let goto_leaf dt t k =
+let goto_leaf trie t k =
   (* the root of the tree *)
-  let root = dt in
+  let root = trie in
   (* function to go to the given leaf, building it if needed *)
-  let rec goto dt t rebuild =
-    match dt, t with
+  let rec goto trie t rebuild =
+    match trie, t with
     | (Leaf l) as leaf, [] -> (* found leaf *)
       (match k l with
-      | Leaf [] -> rebuild (Leaf [])
       | new_leaf when leaf == new_leaf -> root  (* no change, return same tree *)
       | new_leaf -> rebuild new_leaf)           (* replace by new leaf *)
-    | Node l, c::t' ->
-      search_list [] l c t' rebuild  (* search a subnode that matches *)
+    | Node m, c::t' ->
+      (try  (* insert in subtrie *)
+        let subtrie = T.TMap.find c m in
+        let rebuild' subtrie = match subtrie with
+          | _ when empty_trie subtrie -> rebuild (Node (T.TMap.remove c m))
+          | _ -> rebuild (Node (T.TMap.add c subtrie m))
+        in
+        goto subtrie t' rebuild'
+      with Not_found -> (* no subtrie found *)
+        let subtrie = if t' = [] then Leaf [] else Node T.TMap.empty
+        and rebuild' subtrie = match subtrie with
+          | _ when empty_trie subtrie -> rebuild (Node (T.TMap.remove c m))
+          | _ -> rebuild (Node (T.TMap.add c subtrie m))
+        in
+        goto subtrie t' rebuild')
     | Node _, [] -> assert false (* ill-formed term *)
-    | Leaf, _ -> assert false  (* wrong arity *)
-  (* traverse the list of subtries to find one that match c *)
-  and search_list left right c t' rebuild =
-    match right with
-    | [] ->
-      (* no subtrie found. Continue with empty tree; If something is returned,
-         build a node that points to it *)
-      let rebuild' subtrie =
-        let l = match subtrie with
-          | Leaf [] | Node [] -> List.rev left
-          | _ -> List.rev ((c, subtrie)::left)
-        in rebuild (Node l) in
-        rebuild (Node l)
-      and empty_trie = if t' = [] then Leaf [] else Node [] in
-      goto empty_trie t' rebuild'
-    | (c', subtrie)::right' when eq_character c c' ->
-      (* subtrie found, go into it. If it changes, just rebuild *)
-      let rebuild' subtrie =
-        let l = match subtrie with
-          | Leaf [] | Node [] -> (List.rev left) @ right
-          | _ -> (List.rev ((c, subtrie)::left)) @ right
-        in rebuild (Node l) in
-      goto subtrie t' rebuild'
-    | c'::right' ->
-      (* continue to the next subtrie*)
-      search_list (c'::left) right' c t' rebuild
+    | Leaf _, _ -> assert false  (* wrong arity *)
   in
-  goto dt t (fun t -> t)
+  goto trie t (fun t -> t)
       
 (** the tree itself, with metadata *)
 type 'a dtree = {
-  size : int;
   min_var : int;
   max_var : int;
   cmp : 'a -> 'a -> bool;
@@ -158,11 +131,10 @@ type 'a dtree = {
 
 (** empty discrimination tree (with a comparison function) *)
 let empty f = {
-  size = 0;
   min_var = max_int;
   max_var = min_int;
   cmp = f;
-  tree = Node [];
+  tree = Node T.TMap.empty;
 }
 
 (** add a term and a value to the discrimination tree. The priority
@@ -170,28 +142,27 @@ let empty f = {
     are iterated on the first). *)
 let add dt ?(priority=0) t v =
   let chars = to_list t in
-  let k leaf = match leaf with
-    | Leaf l ->
-      let l' = ((t, v, priority)::l in
-      let l' = List.stable_sort (fun (_, _, p1) (_, _, p2) -> p1 - p2) in
-      Leaf l'
-    | _ -> assert false
+  let k l =
+    let l' = (t, v, priority)::l in
+    Leaf (List.stable_sort (fun (_, _, p1) (_, _, p2) -> p1 - p2) l')
   in
-  let tree = goto_leaf dt.tree chars k in
-  {dt with tree=tree}  (* TODO update max var and min var *)
+  let tree = goto_leaf dt.tree chars k
+  and max_var = max (T.max_var t.vars) dt.max_var
+  and min_var = min (T.min_var t.vars) dt.min_var in
+  {dt with tree; max_var; min_var;}
 
 (** remove the term -> value from the tree *)
 let remove dt t v =
   let chars = to_list t in
-  let k leaf = match leaf with
-    | Leaf l ->
-      (* remove tuples that match *)
-      let l' = List.filter (fun (t', v', _) -> t' != t || not (dt.cmp v v')) l in
-      Leaf l'
-    | _ -> assert false
+  let k l =
+    (* remove tuples that match *)
+    let l' = List.filter (fun (t', v', _) -> t' != t || not (dt.cmp v v')) l in
+    Leaf l'
   in
   let tree = goto_leaf dt.tree chars k in
-  {dt with tree=tree}   (* TODO update max var and min var *)
+  (* we assume the (term->value) was in the tree; we also do not
+    update max and min vars, so they are an approximation *)
+  {dt with tree;}
 
 (** maximum variable in the tree *)
 let min_var dt = dt.min_var
@@ -200,19 +171,79 @@ let min_var dt = dt.min_var
 let max_var dt = dt.max_var
 
 (** iterate on all (term -> value) such that subst(term) = input_term *)
-let iter_match dt t k = failwith "not implemented"  (* TODO *)
+let iter_match dt t k =
+  (* variable collision check *)
+  assert (T.is_ground_term t || T.max_var t.vars < dt.min_var || T.min_var t.vars > dt.max_var);
+  (* recursive traversal of the trie, following paths compatible with t *)
+  let rec traverse trie pos subst =
+    match trie with
+    | Leaf l ->  (* yield all answers *)
+      List.iter (fun (t', v, _) -> k t' v subst) l
+    | Node m ->
+      (* "lazy" transformation to flatterm *)
+      let t_pos = get_pos t pos in
+      let t1 = term_to_char t_pos in
+      T.TMap.iter
+        (fun t1' subtrie ->
+          (* explore branch that has the same symbol, if any *)
+          (if T.eq_term t1' t1 then (assert (not (T.is_var t1));
+                                     traverse subtrie (next t pos) subst));
+          (* if variable, try to bind it and continue *)
+          (if T.is_var t1' && t1'.sort = t_pos.sort && S.is_in_subst t1' subst
+            then  (* already bound, check consistency *)
+              let t_matched = T.expand_bindings t_pos
+              and t_bound = T.expand_bindings t1'.binding in
+              if T.eq_term t_matched t_bound
+                then traverse subtrie (skip t pos) subst  (* skip term *)
+                else () (* incompatible bindings of the variable *)
+            else if T.is_var t1' && t1'.sort = t_pos.sort
+              then begin
+                (* t1' not bound, so we bind it and continue in subtree *)
+                T.set_binding t1' (T.expand_bindings t_pos);
+                let subst' = S.update_binding subst t1' in
+                traverse subtrie (skip t pos) subst';
+                T.reset_binding t1'  (* cleanup the variable *)
+              end))
+        m
+  in
+  T.reset_vars t;
+  traverse dt.tree 0 S.id_subst
 
 (** iterate on all (term -> value) in the tree *)
 let iter dt k =
-  let rec iter dt =
-    match dt with
-    | Node l -> List.iter (fun (_, sub_dt) -> iter sub_dt) l
+  let rec iter trie =
+    match trie with
+    | Node m -> T.TMap.iter (fun _ sub_dt -> iter sub_dt) m
     | Leaf l -> List.iter (fun (t, v, _) -> k t v) l
-  in iter dt
+  in iter dt.tree
   
 
 (* --------------------------------------------------------
  * pretty printing
  * -------------------------------------------------------- *)
 
-let pp_tree formatter t = failwith "not implemented"
+module PrintTree = Prtree.Make(
+  struct
+    type t = string * term trie
+
+    (* get a list of (key, sub-node) *)
+    let get_values map =
+      let l : t list ref = ref [] in
+      T.TMap.iter
+        (fun key node -> 
+          let key_repr = Utils.sprintf "[@[<h>%a@]]" !T.pp_term#pp key in
+          l := (key_repr, node) :: !l) map;
+      !l
+    and pp_rule formatter (l, r, _) =
+      Format.fprintf formatter "@[<h>%a → %a@]" !T.pp_term#pp l !T.pp_term#pp r
+
+    (* recurse in subterms *)
+    let decomp (prefix, t) = match t with
+      | Node m -> prefix, get_values m
+      | Leaf l ->
+        let rules_repr = Utils.sprintf "%s @[<h>{%a}@]"
+          prefix (Utils.pp_list ~sep:"; " pp_rule) l in
+        rules_repr, []
+  end)
+
+let pp_term_tree formatter dt = PrintTree.print formatter ("", dt.tree)
--- src/dtree.mli
+++ src/dtree.mli
@@ -27,25 +27,25 @@ type 'a dtree
 val empty : ('a -> 'a -> bool) -> 'a dtree
   (** empty discrimination tree (with a comparison function) *)
 
-val add : dtree -> ?priority:int term -> 'a -> dtree
+val add : 'a dtree -> ?priority:int -> term -> 'a -> 'a dtree
   (** add a term and a value to the discrimination tree. The priority
       is used to sort index values (by increasing number, the lowest
       are iterated on the first). *)
 
-val remove : dtree -> term -> 'a -> dtree
+val remove : 'a dtree -> term -> 'a -> 'a dtree
   (** remove the term -> value from the tree *)
 
-val min_var : dtree -> int
+val min_var : 'a dtree -> int
   (** maximum variable in the tree (surapproximation) *)
 
-val max_var : dtree -> int
+val max_var : 'a dtree -> int
   (** minimum variable in the tree (surapproximation) *)
 
-val iter_match : dtree -> term -> (term -> 'a -> subst -> unit) -> unit
+val iter_match : 'a dtree -> term -> (term -> 'a -> substitution -> unit) -> unit
   (** iterate on all (term -> value) such that subst(term) = input_term *)
 
-val iter : dtree -> (term -> 'a -> unit) -> unit
+val iter : 'a dtree -> (term -> 'a -> unit) -> unit
   (** iterate on all (term -> value) in the tree *)
 
-val pp_tree : Format.formatter -> dtree -> unit
-  (** pretty print in ascii *)
+val pp_term_tree : Format.formatter -> term dtree -> unit
+  (** pretty print a tree of terms in ascii *)
