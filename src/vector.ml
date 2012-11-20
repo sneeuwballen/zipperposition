f33da86e430fc6233486db910cd871729cc0037b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2012-11-20 19:38:16 +0100

    updated vectors module

--- src/vector.ml
+++ src/vector.ml
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 (** Growable, mutable vector *)
 
 (** a vector of 'a. *)
-type 'a vector = {
+type 'a t = {
   mutable size : int;
   mutable capacity : int;
   mutable vec : 'a array;
@@ -37,17 +37,23 @@ let create i =
 (** resize the underlying array so that it can contains the
     given number of elements *)
 let resize v newcapacity =
-  assert (newcapacity > v.capacity);
-  let new_vec = Array.create newcapacity (Obj.magic None) in
-  Array.blit v.vec 0 new_vec 0 v.size;
-  v.vec <- new_vec;
-  v.capacity <- newcapacity
+  if newcapacity <= v.capacity
+    then ()  (* already big enough *)
+    else begin
+      assert (newcapacity >= v.size);
+      let new_vec = Array.create newcapacity (Obj.magic None) in
+      Array.blit v.vec 0 new_vec 0 v.size;
+      v.vec <- new_vec;
+      v.capacity <- newcapacity
+    end
 
 let clear v =
   v.size <- 0;
   if v.capacity > 1000  (* shrink if too large *)
     then (v.capacity <- 10;
-          v.vec <- Array.create i (Obj.magic None))
+          v.vec <- Array.create 10 (Obj.magic None))
+
+let is_empty v = v.size = 0
 
 let push v x =
   (if v.capacity = v.size
@@ -55,36 +61,81 @@ let push v x =
   v.vec.(v.size) <- x;
   v.size <- v.size + 1
 
+(** add all elements of b to a *)
+let append a b =
+  resize a (a.size + b.size);
+  Array.blit b.vec 0 a.vec a.size b.size;
+  a.size <- a.size + b.size
+
+let append_array a b =
+  resize a (a.size + Array.length b);
+  Array.blit b 0 a.vec a.size (Array.length b);
+  a.size <- a.size + Array.length b
+
 let pop v =
   (if v.size = 0 then failwith "Vector.pop on empty vector");
   v.size <- v.size - 1;
   let x = v.vec.(v.size) in
   x
 
+let copy v =
+  let v' = create v.size in
+  Array.blit v.vec 0 v'.vec 0 v.size;
+  v'.size <- v.size;
+  v'
+
 let shrink v n =
   if n <= v.size then () else v.size <- n
 
-val iter v k =
+let member ?(cmp=(=)) v x =
+  try
+    for i = 0 to v.size-1 do
+      if cmp x v.vec.(i) then raise Exit
+    done;
+    false
+  with Exit -> true
+
+let sort ?(cmp=compare) v =
+  (* copy vector (to avoid junk in the array), then sort the array *)
+  let v' = copy v in
+  Array.fast_sort cmp v'.vec;
+  v'
+
+let uniq_sort ?(cmp=compare) v =
+  let v' = sort ~cmp v in
+  (* traverse to remove duplicates. i= current index,
+     j=current append index, j<=i *)
+  let rec traverse prev i j =
+    if i = v'.size then v' (* done traversing *)
+    else if cmp prev v.vec.(i) = 0
+      then (v'.size <- v'.size - 1; traverse prev (i+1) j) (* duplicate, remove it *)
+      else (v.vec.(j) <- v.vec.(i); traverse v.vec.(i) (i+1) (j+1)) (* keep it *)
+  in
+  if v'.size = 0
+    then v'
+    else traverse v'.vec.(0) 1 1  (* start at 1, to get the first element in hand *)
+
+let iter v k =
   for i = 0 to v.size -1 do
     k v.vec.(i)
   done
 
-val iteri v k =
+let iteri v k =
   for i = 0 to v.size -1 do
     k i v.vec.(i)
   done
 
-val map v f =
+let map v f =
   let v' = create v.size in
   for i = 0 to v.size - 1 do
-    push v' (f (get v i));
+    push v' (f v.vec.(i));
   done;
   v'
 
 let fold v acc f =
   let acc = ref acc in
   for i = 0 to v.size - 1 do
-    acc := f acc (get v i);
+    acc := f !acc v.vec.(i);
   done;
   !acc
 
@@ -101,17 +152,14 @@ let size v = v.size
 let from_array a =
   let c = Array.length a in
   let v = create c in
-  for i = 0 to c-1 do
-    push v a.(i);
-  done;
+  Array.blit a 0 v.vec 0 c;
+  v.size <- c;
   v
 
 let from_list l =
-  let v = Array.create 10 in
-  let rec add l = match l with
-  | [] -> v
-  | x::l' -> (push v x; add l')
-  in add l
+  let v = create 10 in
+  List.iter (push v) l;
+  v
 
 let to_array v =
   let a = Array.create v.size (Obj.magic None) in
@@ -121,6 +169,6 @@ let to_array v =
 let to_list v =
   let l = ref [] in
   for i = 0 to v.size - 1 do
-    l = get v i :: !l;
+    l := get v i :: !l;
   done;
   List.rev !l
--- src/vector.mli
+++ src/vector.mli
@@ -20,50 +20,67 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 (** Growable, mutable vector *)
 
-type 'a vector
+type 'a t
   (** the type of a vector of 'a *)
 
-val create : int -> 'a vector
+val create : int -> 'a t
   (** create a vector of given initial capacity *)
 
-val clear : 'a vector -> unit
+val clear : 'a t -> unit
   (** clear the content of the vector *)
 
-val is_empty : 'a vector -> bool
+val is_empty : 'a t -> bool
   (** is the vector empty? *)
 
-val push : 'a vector -> 'a -> unit
+val push : 'a t -> 'a -> unit
   (** add an element at the end of the vector *)
 
-val pop : 'a vector -> 'a
+val append : 'a t -> 'a t -> unit
+  (** [append a b] adds all elements of b to a *)
+val append_array : 'a t -> 'a array -> unit
+  (** same as append, with an array *)
+
+val pop : 'a t -> 'a
   (** remove last element, or raise a Failure if empty *)
 
-val shrink : 'a vector -> int -> unit
+val copy : 'a t -> 'a t
+  (** shallow copy *)
+
+val shrink : 'a t -> int -> unit
   (** shrink to the given size (remove elements above this size) *)
 
-val iter : 'a vector -> ('a -> unit) -> unit
+val member : ?cmp:('a -> 'a -> bool) -> 'a t -> 'a -> bool
+  (** is the element a member of the vector? *)
+
+val sort : ?cmp:('a -> 'a -> int) -> 'a t -> 'a t
+  (** sort the array *)
+
+val uniq_sort : ?cmp:('a -> 'a -> int) -> 'a t -> 'a t
+  (** sort the array and remove duplicates *)
+
+val iter : 'a t -> ('a -> unit) -> unit
   (** iterate on the vector *)
 
-val iteri : 'a vector -> (int -> 'a -> unit) -> unit
+val iteri : 'a t -> (int -> 'a -> unit) -> unit
   (** iterate on the vector with indexes *)
 
-val map : 'a vector -> ('a -> 'b) -> 'b vector
+val map : 'a t -> ('a -> 'b) -> 'b t
   (** map elements of the vector *)
 
-val fold : 'a vector -> 'b -> ('b -> 'a -> 'b) -> 'b
+val fold : 'a t -> 'b -> ('b -> 'a -> 'b) -> 'b
   (** fold on elements of the vector *)
 
-val get : 'a vector -> int -> 'a
+val get : 'a t -> int -> 'a
   (** access element, or Failure if bad index *)
 
-val set : 'a vector -> int -> 'a -> unit
+val set : 'a t -> int -> 'a -> unit
   (** access element, or Failure if bad index *)
 
-val size : 'a vector -> int
+val size : 'a t -> int
   (** number of elements in vector *)
 
-val from_array : 'a array -> 'a vector
-val from_list : 'a list -> 'a vector
-val to_array : 'a vector -> 'a array
-val to_list : 'a vector -> 'a list
+val from_array : 'a array -> 'a t
+val from_list : 'a list -> 'a t
+val to_array : 'a t -> 'a array
+val to_list : 'a t -> 'a list
 
