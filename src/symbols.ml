10c0af0e53e1b67ab7bd23379fde02219118a61b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2012-11-26 14:54:38 +0100

    forgot the symbols modules; small change in superposition w.r.t. De Bruijn terms (because in some cases we do not want to lift symbols, e.g. when replacing)

---
+++ src/symbols.ml
@@ -0,0 +1,119 @@
+(*
+Zipperposition: a functional superposition prover for prototyping
+Copyright (C) 2012 Simon Cruanes
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+*)
+
+(** Symbols and signature *)
+
+type symbol = string
+
+let compare_symbols s1 s2 = String.compare s1 s2
+
+let hash_symbol s = Hashtbl.hash s
+
+(** weak hash table for symbols *)
+module HashSymbol = Weak.Make(
+  struct
+    type t = string
+    let equal s1 s2 = s1 = s2
+    let hash = hash_symbol
+  end)
+
+(** the global symbol table *)
+let symb_table = HashSymbol.create 17
+
+let sig_version = ref 0
+
+let mk_symbol s =
+  let s' = HashSymbol.merge symb_table s in
+  (if s' == s then incr sig_version); (* update signature *)
+  s'
+
+let name_symbol s = s
+
+module SHashtbl = Hashtbl.Make(
+  struct
+    type t = symbol
+    let equal = (==)
+    let hash s = hash_symbol s
+  end)
+
+module SHashSet =
+  struct
+    type t = unit SHashtbl.t
+    let create () = SHashtbl.create 7
+    let member t s = SHashtbl.mem t s
+    let add t s = SHashtbl.replace t s ()
+    let from_list ss =
+      let t = create () in
+      List.iter (add t) ss;
+      t
+  end
+
+
+(* connectives *)
+let true_symbol = mk_symbol "$true"
+let false_symbol = mk_symbol "$false"
+let eq_symbol = mk_symbol "="
+let exists_symbol = mk_symbol "$$exists"
+let forall_symbol = mk_symbol "$$forall"
+let lambda_symbol = mk_symbol "$$lambda"
+let not_symbol = mk_symbol"$$not"
+let imply_symbol = mk_symbol "$$imply"
+let and_symbol = mk_symbol "$$and"
+let or_symbol = mk_symbol "$$or"
+
+(* De Bruijn *)
+let db_symbol = mk_symbol "$$db"
+let succ_db_symbol = mk_symbol "$$s"
+
+type sort = symbol
+
+(* default sorts *)
+let type_sort = mk_symbol "$tType"
+let bool_sort = mk_symbol "$o"
+let univ_sort = mk_symbol "$i"
+
+let table =
+  [true_symbol, bool_sort, 0;
+   false_symbol, bool_sort, 0;
+   eq_symbol, bool_sort, 2;
+   exists_symbol, bool_sort, 1;
+   forall_symbol, bool_sort, 1;
+   lambda_symbol, univ_sort, 1;
+   not_symbol, bool_sort, 1;
+   imply_symbol, bool_sort, 2;
+   and_symbol, bool_sort, 2;
+   or_symbol, bool_sort, 2;
+   db_symbol, univ_sort, 0;
+   succ_db_symbol, univ_sort, 1;
+   ]
+
+(** default signature, containing predefined symbols with their arities and sorts *)
+let base_signature () =
+  let sorts = SHashtbl.create 23
+  and arities  = SHashtbl.create 23
+  and symbols = ref [] in
+  (* update the tables *)
+  List.iter
+    (fun (symb, sort, arity) ->
+      SHashtbl.add sorts symb sort;
+      SHashtbl.add arities symb arity;
+      symbols := symb :: !symbols)
+    table;
+  sorts, arities, !symbols
---
+++ src/symbols.mli
@@ -0,0 +1,78 @@
+(*
+Zipperposition: a functional superposition prover for prototyping
+Copyright (C) 2012 Simon Cruanes
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+*)
+
+(** Symbols and signature *)
+
+type symbol
+  (** abstract type of a symbol *)
+
+type sort = symbol
+  (** a sort for terms (only the return sort is kept) *)
+
+val compare_symbols : symbol -> symbol -> int
+  (** total ordering on symbols (equality is ==) *)
+
+val hash_symbol : symbol -> int
+  (** hash the symbol *)
+
+val mk_symbol : string -> symbol
+  (** construction of a symbol *)
+
+val name_symbol : symbol -> string
+  (** deconstruction of a symbol *)
+
+val sig_version : int ref
+  (** current version of the signature (updated upon symbol creation) *)
+
+module SHashtbl : Hashtbl.S with type key = symbol
+
+(** Hashset for symbols *)
+module SHashSet :
+  sig
+    type t
+    val create : unit -> t
+    val member : t -> symbol -> bool
+    val add : t -> symbol -> unit
+    val from_list : symbol list -> t
+  end
+
+(* connectives *)
+val true_symbol : symbol
+val false_symbol : symbol
+val eq_symbol : symbol
+val exists_symbol : symbol
+val forall_symbol : symbol
+val lambda_symbol : symbol
+val not_symbol : symbol
+val imply_symbol : symbol
+val and_symbol : symbol
+val or_symbol : symbol
+
+(* De Bruijn *)
+val db_symbol : symbol
+val succ_db_symbol : symbol
+
+(* sorts *)
+val bool_sort : sort
+val type_sort : sort
+val univ_sort : sort
+
+val base_signature : unit -> (sort SHashtbl.t * int SHashtbl.t * symbol list)
+  (** the signature composed of predefined symbols *)
