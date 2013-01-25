a7878fb336dda6987afe651c2ccd31494435a9cc
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-01-25 13:03:11 +0100

    namespaced Bitvector. Cleaner.

--- src/bitvector.ml
+++ src/bitvector.ml
@@ -21,22 +21,25 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 (* bitvectors of at most 31 bits *)
 
 (** a bitvector is just an int *)
-type bitvector = int
+type t = int
 
 (** make a bitvector of size n with all bits set *)
-let bv_make n =
+let make n =
   assert (n <= 31);
   let rec shift bv n = if n = 0 then bv else shift ((bv lsl 1) lor 1) (n-1)
   in shift 0 n
 
+(** Empty bitvector *)
+let empty = 0
+
 (** bitvector n-th element is true? *)
-let bv_get bv n = (bv land (1 lsl n)) <> 0 
+let get bv n = (bv land (1 lsl n)) <> 0 
 
 (** set n-th element of bitvector *)
-let bv_set bv n = bv lor (1 lsl n)
+let set bv n = bv lor (1 lsl n)
 
 (** reset n-th element of bitvector *)
-let bv_clear bv n = bv land (lnot (1 lsl n))
+let clear bv n = bv land (lnot (1 lsl n))
 
 (** is bitvector empty? *)
-let bv_empty bv = bv = 0
+let is_empty bv = bv = 0
