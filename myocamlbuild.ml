09c037d7b4e105ca71a0c6f6af94adb879dccb3f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-08-30 12:10:50 +0200

    sublibrary, Logtk_meta, for the meta-prover. It depends on datalog and is enabled by default. Logtk itself no longer depends on datalog.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: eb7ed1b99f09a569a7e783e541d403d1) *)
+(* DO NOT EDIT (digest: f9ede792303ad0c59e5564534b883c9b) *)
 module OASISGettext = struct
 (* # 21 "src/oasis/OASISGettext.ml" *)
 
@@ -477,7 +477,8 @@ end
 open Ocamlbuild_plugin;;
 let package_default =
   {
-     MyOCamlbuildBase.lib_ocaml = [("logtk", ["src"; "src/lib"])];
+     MyOCamlbuildBase.lib_ocaml =
+       [("logtk", ["src"; "src/lib"]); ("logtk_meta", ["src"])];
      lib_c = [];
      flags = [];
      includes =
@@ -492,6 +493,6 @@ let package_default =
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 496 "myocamlbuild.ml"
+# 497 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
