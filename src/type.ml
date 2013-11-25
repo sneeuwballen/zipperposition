6e9052322067e2443a7add2b0d1652a6c162c478
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-11-25 11:27:37 +0100

    more informative errors in Type.apply; rewritten part of the hashconsing

--- src/type.ml
+++ src/type.ml
@@ -47,36 +47,42 @@ type ty = t
 exception Error of string
   (** Generic error on types. *)
 
-let rec eq_struct t1 t2 = match t1.ty, t2.ty with
-  | Var i1, Var i2 -> i1 = i2
-  | BVar i1, BVar i2 -> i1 = i2
-  | App (s1, l1), App (s2, l2) -> 
-    s1 = s2 && (try List.for_all2 (==) l1 l2 with Invalid_argument _ -> false)
-  | Fun (ret1,l1), Fun (ret2,l2) ->
-    ret1 == ret2 && (try List.for_all2 (==) l1 l2 with Invalid_argument _ -> false)
-  | Forall ty1, Forall ty2 -> ty1 == ty2
-  | _, _ -> false
+let eq t1 t2 = t1 == t2
 
 let hash t = t.id
 
-let rec _hash_top t = match t.ty with
+let cmp ty1 ty2 = ty1.id - ty2.id
+
+let rec _eq_list l1 l2 = match l1, l2 with
+  | [], [] -> true
+  | [], _
+  | _, [] -> false
+  | x1::l1', x2::l2' -> eq x1 x2 && _eq_list l1' l2'
+
+let _eq_top t1 t2 = match t1.ty, t2.ty with
+  | Var i1, Var i2 
+  | BVar i1, BVar i2 -> i1 = i2
+  | App (s1, l1), App (s2, l2) -> 
+    s1 = s2 && _eq_list l1 l2
+  | Fun (ret1,l1), Fun (ret2,l2) ->
+    eq ret1 ret2 && _eq_list l1 l2
+  | Forall ty1, Forall ty2 -> eq ty1 ty2
+  | _, _ -> false
+
+let _hash_top t = match t.ty with
   | Var i -> i
-  | BVar i -> Hash.hash_int i
+  | BVar i -> Hash.combine 33 i
   | App (s, l) -> Hash.hash_list hash (Hash.hash_string s) l
   | Fun (ret, l) -> Hash.hash_list hash (hash ret) l
-  | Forall ty -> Hash.hash_int (hash ty)
+  | Forall ty ->  Hash.combine 12 (hash ty)
 
 (* hashconsing *)
 module H = Hashcons.Make(struct
   type t = ty
-  let equal = eq_struct
+  let equal = _eq_top
   let hash = _hash_top
-  let tag i ty = ty.id <- i
+  let tag i ty = (assert (ty.id = ~-1); ty.id <- i)
 end)
-
-let eq t1 t2 = t1 == t2
-
-let cmp ty1 ty2 = ty1.id - ty2.id
 
 let is_var = function | {ty=Var _} -> true | _ -> false
 let is_bvar = function | {ty=BVar _} -> true | _ -> false
@@ -106,11 +112,11 @@ let _mk_fun ret args =
   let ty = {ty=Fun (ret, args); id= ~-1; ground=false;} in
   let ty' = H.hashcons ty in
   if ty == ty' then begin
-    ty.ground <- ret.ground && _list_ground args
+    ty'.ground <- ret.ground && _list_ground args
     end;
   ty'
 
-let rec (<==) ret args =
+let rec mk_fun ret args =
   match args with
   | [] -> ret
   | _::_ ->
@@ -120,22 +126,24 @@ let rec (<==) ret args =
           constructor invariant. [args] must be applied before [args']
           need to be supplied.
           Example: [(a <- b) <- c] requires [c] first *)
-      ret' <== (args @ args')
+      mk_fun ret' (args @ args')
     | _ -> _mk_fun ret args
 
-let (<=.) ret arg = ret <== [arg]
+let (<==) = mk_fun
 
-let (@@) s args =
-  let ty = {ty=App(s, args); id= ~-1; ground=false} in
-  let ty' = H.hashcons ty in
-  if ty == ty' then begin
-    ty.ground <- _list_ground args;
+let (<=.) ret arg = mk_fun ret [arg]
+
+let app s args =
+  let my_ty = {ty=App(s, args); id= ~-1; ground=false} in
+  let ty' = H.hashcons my_ty in
+  if my_ty == ty' then begin
+    ty'.ground <- _list_ground args;
     end;
   ty'
 
-let const s = s @@ []
+let const s = app s []
 
-let app s args = s @@ args
+let (@@) = app
 
 let var i =
   if i < 0 then failwith "Type.var: expected a nonnegative int";
@@ -145,11 +153,9 @@ let bvar i =
   if i < 0 then failwith "Type.bvar: expected a nonnegative int";
   H.hashcons {ty=BVar i; id= ~-1; ground=false; }
 
-let mk_fun = (<==)
-
 (* real constructor *)
 let __forall ty =
-  H.hashcons {ty=Forall ty; id= ~-1; ground=false; }
+  H.hashcons {ty=Forall ty; id= ~-1; ground=ty.ground; }
 
 (** Handling De Bruijn indexes. We assume that all types are
     always {!DB.closed}, ie all De Bruijn indices are properly bound
@@ -295,38 +301,6 @@ let rec size ty = match ty.ty with
 
 let _error msg = raise (Error msg)
 
-(* apply a type to arguments. *)
-let apply ty args =
-  (* apply (eval env ty) to (eval env args) *)
-  let rec apply ~env ty args =
-    match ty.ty, args with
-    | _, [] -> DB.eval env ty
-    | Fun (ret, l), l' -> apply_fun ~env ret l l'
-    | Forall ty', a::args' ->
-      let env = DBEnv.push env a in
-      apply ~env ty' args'
-    | _, _ -> _error "Type.apply: expected function or forall type"
-  (* recursive matching of expected arguments and provided arguments.
-    careful: we could have a curried function *)
-  and apply_fun ~env f_ret f_args args = match f_args, args with
-    | x::f_args', y::args' ->
-      (* match arguments after substitution *)
-      if eq (DB.eval env x) (DB.eval env y)
-        then apply_fun ~env f_ret f_args' args'
-        else _error "Type.apply: argument type mismatch"
-    | [], [] ->
-      (* total application, evaluate the return type. special case of last case. *)
-      DB.eval env f_ret
-    | [], _ -> apply ~env f_ret args
-    | _::_, [] ->
-      (* partial application. The remaining arguments will have to be
-          provided by another call to {!apply}. *)
-      mk_fun (DB.eval env f_ret) (DB.eval_list env f_args)
-  in
-  apply ~env:DBEnv.empty ty args
-
-(** {2 IO} *)
-
 let rec pp_rec depth buf t = match t.ty with
   | Var i -> Printf.bprintf buf "T%d" i
   | BVar i -> Printf.bprintf buf "Tb%i" (depth-i-1)
@@ -344,6 +318,42 @@ and pp_inner depth buf t = match t.ty with
   | _ -> pp_rec depth buf t
 
 let pp buf t = pp_rec 0 buf t
+
+(* apply a type to arguments. *)
+let apply ty args =
+  (* apply ty to args *)
+  let rec apply ty args =
+    match ty.ty, args with
+    | _, [] -> ty
+    | Fun (ret, l), l' -> apply_fun ret l l'
+    | Forall ty', a::args' ->
+      let ty' = DB.eval (DBEnv.singleton a) ty' in
+      apply ty' args'
+    | _, _ -> _error "Type.apply: expected function or forall type"
+  (* recursive matching of expected arguments and provided arguments.
+    careful: we could have a curried function *)
+  and apply_fun f_ret f_args args =
+    match f_args, args with
+    | x::f_args', y::args' ->
+      (* match arguments after substitution *)
+      if eq x y
+        then apply_fun f_ret f_args' args'
+        else
+          let msg = Util.sprintf
+            "Type.apply: argument type mismatch, expected %a[%d] but got %a[%d]" pp x x.id pp y y.id in
+          _error msg
+    | [], [] ->
+      (* total application, result is return type. special case of last case. *)
+      f_ret
+    | [], _ -> apply f_ret args
+    | _::_, [] ->
+      (* partial application. The remaining arguments will have to be
+          provided by another call to {!apply}. *)
+      mk_fun f_ret f_args
+  in
+  apply ty args
+
+(** {2 IO} *)
 
 let rec pp_tstp_rec depth buf t = match t.ty with
   | Var i -> Printf.bprintf buf "T%d" i
