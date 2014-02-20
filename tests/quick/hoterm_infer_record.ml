7a61e902201005c080f780a91e6837ca20ca99cf
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-02-20 17:56:16 +0100

    update of tests

--- tests/quick/hoterm_infer_record.ml
+++ tests/quick/hoterm_infer_record.ml
@@ -6,12 +6,17 @@ let ctx = TypeInference.Ctx.create
     [~< "f",
     Type.(TPTP.i <== [record ["x", TPTP.int; "y", TPTP.o] ~rest:(Some (var 1));
                       TPTP.int ])]);;
+
 let t = PT.app
   (PT.const ~< "f")
     [ PT.record ["x", PT.of_int 42] ~rest:(Some (PT.var "R"));
       PT.var "X"];;
 
 (* tough type inference *)
-let t' = TypeInference.HO.convert ctx t;;
+let t' = TypeInference.HO.convert ~ret:Type.TPTP.i ~generalize:false ~ctx t;;
+
+(* tougher type inference *)
+TypeInference.Ctx.clear ctx;;
+let t'' = TypeInference.HO.convert ~ret:Type.TPTP.i ~generalize:true ~ctx t;;
 
 print_endline "... OK";;
