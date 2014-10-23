8ea8d0a751703345e89b827fdd5aeaba01b351df
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-10-23 11:33:54 +0200

    removed old files, including Bij,Bencode and some Meta{KB,Prover,...}; removed some deprecation warnings

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 661ce1c19ff541f814408c62f980ebee) *)
+(* DO NOT EDIT (digest: 30d64a9132fcc31c90893f33b878458a) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -630,6 +630,8 @@ let package_default =
           ("src/solving", ["src/base"; "src/base/lib"]);
           ("src/parsers", ["src/base"; "src/base/lib"]);
           ("src/meta", ["src/base"; "src/base/lib"; "src/parsers"]);
+          ("src/demo/resolution",
+            ["src/base"; "src/base/lib"; "src/parsers"]);
           ("src/base/lib", ["src/base"]);
           ("src/base", ["src/base/lib"]);
           ("src/arbitrary", ["src/base"; "src/base/lib"; "src/meta"])
@@ -639,7 +641,7 @@ let package_default =
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 643 "myocamlbuild.ml"
+# 645 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
 
--- src/base/lib/cache.ml
+++ src/base/lib/cache.ml
@@ -123,7 +123,7 @@ module Linear(X : EQ) = struct
 
   let create size =
     assert (size >= 1);
-    Array.create size Empty
+    Array.make size Empty
 
   let clear cache =
     Array.fill cache 0 (Array.length cache) Empty
@@ -166,7 +166,7 @@ module Linear2(X : EQ)(Y : EQ) = struct
 
   let create size =
     assert (size >= 1);
-    Array.create size Empty
+    Array.make size Empty
 
   let clear cache =
     Array.fill cache 0 (Array.length cache) Empty
@@ -214,7 +214,7 @@ module Replacing(X : HASH) = struct
   and 'a bucket = Empty | Assoc of key * 'a
 
   let create size =
-    Array.create size Empty
+    Array.make size Empty
 
   let clear c =
     Array.fill c 0 (Array.length c) Empty
@@ -245,7 +245,7 @@ module Replacing2(X : HASH)(Y : HASH) = struct
 
 
   let create size =
-    Array.create size Empty
+    Array.make size Empty
 
   let clear c =
     Array.fill c 0 (Array.length c) Empty
