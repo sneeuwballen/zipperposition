944fa8dc3e48d042a866689de2d600192a0b633a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-10-16 11:55:43 +0200

    new module `ClauseQueue_intf`

--- src/clauseQueue.ml
+++ src/clauseQueue.ml
@@ -38,116 +38,7 @@ module O = Ordering
 module Lit = Literal
 module Lits = Literals
 
-(** {2 A priority queue of clauses, purely functional} *)
-module type S = sig
-  module C : Clause.S
-
-  (** {6 Weight functions} *)
-  module WeightFun : sig
-    type t = C.t -> int
-    (** attribute a weight to a clause. The smaller, the better (lightweight
-        clauses will be favored). A weight must always be positive;
-        the weight of the empty clause should alwyays be 0. *)
-
-    val default : t
-    (** Use {!Literal.heuristic_weight} *)
-
-    val age : t
-    (** Returns the age of the clause (or 0 for the empty clause) *)
-
-    val favor_conjecture : t
-    (** The closest a clause is from conjectures, the lowest its weight.
-        Some threshold is used for clauses that are too far away *)
-
-    val combine : (t * int) list -> t
-    (** Combine a list of pairs [w, coeff] where [w] is a weight function,
-        and [coeff] a strictly positive number. This is a weighted sum
-        of weights. *)
-  end
-
-  type t
-  (** A priority queue. *)
-
-  val add : t -> C.t -> t
-    (** Add a clause to the Queue *)
-
-  val adds : t -> C.t Sequence.t -> t
-    (** Add clauses to the queue *)
-
-  val is_empty : t -> bool
-    (** check whether the queue is empty *)
-
-  val take_first : t -> (t * C.t)
-    (** Take first element of the queue, or raise Not_found *)
-
-  val clean : t -> C.CSet.t -> t
-    (** remove all clauses that are not in the set *)
-
-  val name : t -> string
-    (** Name of the implementation/role of the queue *)
-
-  (** {6 Available Queues} *)
-
-  val fifo : t
-    (** select by increasing age (for fairness) *)
-
-  val clause_weight : t
-    (** select by increasing weight of clause *)
-
-  val goals : t
-    (** only select goals (clauses with only negative lits) *)
-
-  val non_goals : t
-    (** only select non-goals *)
-
-  val ground : t
-    (** only select ground clauses *)
-
-  val pos_unit_clauses : t
-    (** only select positive unit clauses *)
-
-  val horn : t
-    (** select horn clauses *)
-
-  val lemmas : t
-    (** only select lemmas *)
-
-  val goal_oriented : t
-    (** custom weight function that favors clauses that are "close" to
-        initial conjectures. It is fair.  *)
-
-  val mk_queue : ?accept:(C.t -> bool) -> weight:(C.t -> int) -> string -> t
-    (** Bring your own implementation of queue *)
-
-  (** {6 Combination of queues} *)
-
-  type queues = (t * int) list
-
-  module Profiles : sig
-    val bfs : queues
-      (** Strong orientation toward FIFO *)
-
-    val explore : queues
-      (** Use heuristics for selecting "small" clauses *)
-
-    val ground : queues
-      (** Favor positive unit clauses and ground clauses *)
-
-    val why3 : queues
-      (** Optimized for why3 *)
-  end
-
-  val default_queues : queues
-    (** default combination of heuristics *)
-
-  (** {6 IO} *)
-
-  val pp : Buffer.t -> t -> unit
-  val to_string : t -> string
-  val pp_list : Buffer.t -> (t * int) list -> unit
-  val fmt : Format.formatter -> t -> unit
-  val fmt_list : Format.formatter -> (t * int) list -> unit
-end
+module type S = ClauseQueue_intf.S
 
 let _profile = ref "default"
 let profile () = !_profile
@@ -171,7 +62,13 @@ module Make(C : Clause.S) = struct
   module WeightFun = struct
     type t = C.t -> int
 
+    let weight_lits_ l =
+      Array.fold_left
+        (fun acc lit -> acc + Lit.heuristic_weight term_weight lit)
+        0 l
+
     let default c =
+      (* maximum depth of types. Avoids reasoning on list (list (list .... (list int))) *)
       let _depth_ty =
         Lits.Seq.terms (C.lits c)
         |> Sequence.map FOTerm.ty
@@ -179,10 +76,20 @@ module Make(C : Clause.S) = struct
         |> Sequence.max ?lt:None
         |> CCOpt.maybe CCFun.id 0
       in
-      let w = Array.fold_left
-        (fun acc lit -> acc + Lit.heuristic_weight term_weight lit)
-        0 (C.lits c)
-      in w * Array.length (C.lits c) + _depth_ty
+      let trail = C.get_trail c in
+      let w_lits = weight_lits_ (C.lits c) in
+      let w_trail = C.Trail.fold
+        (fun acc t -> match C.Ctx.BoolLit.extract_exn (C.Ctx.BoolLit.abs t) with
+          | C.Ctx.BoolLit.Clause_component lits -> acc + weight_lits_ lits
+          | C.Ctx.BoolLit.Form f -> acc + Formula.FO.weight f
+          | C.Ctx.BoolLit.Ctx (c,_,_) -> acc + weight_lits_ (ClauseContext.raw_lits c)
+          | C.Ctx.BoolLit.Case (_,_) ->
+              acc + 10 (* generic penalty for each inductive hypothesis *)
+          | C.Ctx.BoolLit.Name _
+          | C.Ctx.BoolLit.Input  -> acc (* mere markers *)
+        ) 0 trail 
+      in
+      w_lits * Array.length (C.lits c) + w_trail * (C.Trail.length trail) + _depth_ty
 
     let age c =
       if C.is_empty c then 0
