80bfbacc6b85a5f56c53feeb7b782ee1097198d1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-11-26 09:50:01 +0100

    fix tests (bis)

--- tests/quick/.common.ml
+++ tests/quick/.common.ml
@@ -22,17 +22,17 @@ module T = FOTerm;;
 module PT = PrologTerm;;
 module Sym = Symbol;;
 let (~<) = Symbol.of_string;;
-#install_printer Symbol.fmt;;
-#install_printer Type.fmt;;
-#install_printer FOTerm.fmt;;
-#install_printer PrologTerm.fmt;;
-#install_printer Formula.FO.fmt;;
-#install_printer HOTerm.fmt;;
-#install_printer Substs.fmt;;
-#install_printer Signature.fmt;;
-#install_printer ParseLocation.fmt;;
-#install_printer Precedence.fmt;;
-#install_printer Ordering.fmt;;
+#install_printer Symbol.pp;;
+#install_printer Type.pp;;
+#install_printer FOTerm.pp;;
+#install_printer PrologTerm.pp;;
+#install_printer Formula.FO.pp;;
+#install_printer HOTerm.pp;;
+#install_printer Substs.pp;;
+#install_printer Signature.pp;;
+#install_printer ParseLocation.pp;;
+#install_printer Precedence.pp;;
+#install_printer Ordering.pp;;
 open Sequence.Infix;;
 open Logtk.Type;;
 module F = Logtk.Formula;;
@@ -41,10 +41,10 @@ module HOT = Logtk.HOTerm;;
 (* optional part: parser *)
 #load "logtk_parsers.cma";;
 open Logtk_parsers;;
-#install_printer Ast_tptp.Untyped.fmt;;
-#install_printer Ast_tptp.Typed.fmt;;
-#install_printer Ast_tptp.fmt_general;;
-#install_printer Ast_tptp.fmt_role;;
+#install_printer Ast_tptp.Untyped.pp;;
+#install_printer Ast_tptp.Typed.pp;;
+#install_printer Ast_tptp.pp_general;;
+#install_printer Ast_tptp.pp_role;;
 let pterm s =
     let t = Parse_tptp.parse_term Lex_tptp.token (Lexing.from_string s) in
     let tyctx = TypeInference.Ctx.create Signature.TPTP.base in
--- tests/quick/type_check_record_1.ml
+++ tests/quick/type_check_record_1.ml
@@ -6,8 +6,9 @@ let ty2 = Type.(const ~< "a" <== [const ~< "a"; record ["x", const ~< "a"] ~rest
 let s = Unif.Ty.unification Type.(apply ty (var 1)) 0 ty2 1;;
 let ty3 = Substs.Ty.apply ~renaming:(Substs.Renaming.create ()) s Type.(apply ty (var 1)) 0;;
 Util.debug 1 "types: %a (%d) and %a (%d)\n"
+  (fun k->k
   Type.pp ty2 (ST.hash (ty2:Type.t:>ST.t))
-  Type.pp ty3 (ST.hash (ty3:Type.t:>ST.t));;
+  Type.pp ty3 (ST.hash (ty3:Type.t:>ST.t)));;
 assert (Type.eq ty2 ty3);;
 
 ok ();;
