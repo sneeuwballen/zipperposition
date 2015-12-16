0aa9093e7f45ab4c84a67f37abfca6175189bb34
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-12-16 22:16:55 +0100

    printing formats

--- src/core/Options.ml
+++ src/core/Options.ml
@@ -1,46 +1,30 @@
 
-(*
-Copyright (c) 2013, Simon Cruanes
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.  Redistributions in binary
-form must reproduce the above copyright notice, this list of conditions and the
-following disclaimer in the documentation and/or other materials provided with
-the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*)
+(* This file is free software, part of Logtk. See file "license" for more details. *)
 
 (** {1 Global CLI options}
 
-Those options can be used by any program that parses command
-line arguments using the standard module {!Arg}. It may modify some
-global parameters, and return a parameter type for other options.
+    Those options can be used by any program that parses command
+    line arguments using the standard module {!Arg}. It may modify some
+    global parameters, and return a parameter type for other options.
 *)
 
 
 type t = {
   stats : bool;  (** statistics *)
-  print_format : string; (** Printing format ("tstp", "debugf"...) *)
+  print_format : [`Debug | `Normal | `TPTP]; (** Printing format *)
 }
 
 let default = {
   stats = false;
-  print_format = "debugf";
+  print_format = `Normal;
 }
+
+let print_format_of_string s =
+  match String.lowercase (String.trim s) with
+  | "tptp" | "tstp" -> `TPTP
+  | "default" | "normal" -> `Normal
+  | "debug" -> `Debug
+  | _ -> failwith ("unknown print format " ^ s)
 
 let _print_types () =
   HOTerm.print_all_types := true;
@@ -51,13 +35,13 @@ let make_other_opts () =
   (* debugf level for every section *)
   Util.Section.iter
   |> Sequence.filter_map
-      (fun (name,sec) ->
-        if name="" then None
-        else Some
-          ("--debug." ^ name, Arg.Int (Util.Section.set_debug sec),
+    (fun (name,sec) ->
+       if name="" then None
+       else Some
+           ("--debug." ^ name, Arg.Int (Util.Section.set_debug sec),
             " debug level for section " ^ name
-          )
-      )
+           )
+    )
   |> Sequence.to_list
 
 let make opts =
@@ -69,9 +53,12 @@ let make opts =
     ; "--bt", Arg.Bool Printexc.record_backtrace, " enable backtraces"
     ; "--mem-limit", Arg.Int Util.set_memory_limit, " memory limit (in MB)"
     ; "--stats", Arg.Unit (fun () -> mod_opt (fun o -> {o with stats=true;})),
-        " gather and print statistics"
-    ; "--print", Arg.String (fun s -> mod_opt (fun o -> {o with print_format=s;} )),
-        " choose printing format for terms and formulas (default \"debugf\")"
+      " gather and print statistics"
+    ; "--print",
+      Arg.String
+        (fun s ->
+           mod_opt (fun o -> {o with print_format=print_format_of_string s;} )),
+      " choose printing format for terms and formulas (default \"debugf\")"
     ] @ make_other_opts ()
   in
   List.sort (fun (x,_,_)(y,_,_) -> String.compare x y) l
--- src/core/Options.mli
+++ src/core/Options.mli
@@ -1,55 +1,32 @@
 
-(*
-Copyright (c) 2013, Simon Cruanes
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.  Redistributions in binary
-form must reproduce the above copyright notice, this list of conditions and the
-following disclaimer in the documentation and/or other materials provided with
-the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*)
+(* This file is free software, part of Logtk. See file "license" for more details. *)
 
 (** {1 Global CLI options}
 
-Those options can be used by any program that parses command
-line arguments using the standard module {!Arg}. It may modify some
-global parameters, and return a parameter type for other options.
+    Those options can be used by any program that parses command
+    line arguments using the standard module {!Arg}. It may modify some
+    global parameters, and return a parameter type for other options.
 *)
 
 type t = {
   stats : bool;  (** statistics *)
-  print_format : string; (** Printing format ("tstp", "debugf"...) *)
+  print_format : [`Debug | `Normal | `TPTP]; (** Printing format *)
 } (** Options that can be set by the user *)
 
 val default : t
-  (** Default options *)
+(** Default options *)
 
 val make : t ref -> (string * Arg.spec * string) list
-  (** Produce of list of options suitable for {!Arg.parse}, that may
-      modify global parameters and the given option reference.
-      After parsing, the reference content will reflect CLI options *)
+(** Produce of list of options suitable for {!Arg.parse}, that may
+    modify global parameters and the given option reference.
+    After parsing, the reference content will reflect CLI options *)
 
 val global : t ref
-  (** Global parameters, can be used as a mutable default *)
+(** Global parameters, can be used as a mutable default *)
 
 val global_opts : (string * Arg.spec * string) list
-  (** Options that modify {!global}.
-      Caution, this might miss some options from modules that aren't registered yet.
-      @deprecated since 0.6.1 , use {!mk_global_opts} instead*)
+(** Options that modify {!global}.
+    Caution, this might miss some options from modules that aren't registered yet.
+    @deprecated since 0.6.1 , use {!mk_global_opts} instead*)
 
 val mk_global_opts : unit -> (string * Arg.spec * string) list
