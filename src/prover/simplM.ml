50b0745980ec47aa8d42d7394c721957cea5beaa
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-01-08 13:34:25 +0100

    new SimplM monad for simplifications

---
+++ src/prover/simplM.ml
@@ -0,0 +1,46 @@
+
+(* This file is free software, part of Zipperposition. See file "license" for more details. *)
+
+(** {1 Simplification Monad} *)
+
+type +'a t = 'a * [ `Same | `New]
+
+let return_same x = x, `Same
+let return_new x = x, `New
+let return = return_same
+
+let get = fst
+let is_new (_,st) = match st with `New -> true | `Same -> false
+let is_same (_,st) = match st with `New -> false | `Same -> true
+
+let map f (x,st) = f x, st
+
+(* chaining simplifications *)
+let (>>=) (c,state) f =
+  let c', state2 = f c in
+  let state_res = match state, state2 with
+    | `Same, `Same -> `Same
+    | `New, _ | _, `New -> `New
+  in
+  c', state_res
+
+let rec app_list l x = match l with
+  | [] -> return_same x
+  | f :: l' -> f x >>= app_list l'
+
+let rec map_l f l = match l with
+  | [] -> return_same []
+  | x :: tail ->
+      f x >>= fun x' ->
+      map_l f tail >>= fun tail' ->
+      return_same (x' :: tail')
+
+let rec fold_l f acc l = match l with
+  | [] -> return_same acc
+  | x :: tail ->
+      f acc x >>= fun acc -> fold_l f acc tail
+
+module Infix = struct
+  let (>>=) = (>>=)
+  let (>|=) x f = map f x
+end
---
+++ src/prover/simplM.mli
@@ -0,0 +1,36 @@
+
+(* This file is free software, part of Zipperposition. See file "license" for more details. *)
+
+(** {1 Simplification Monad}
+
+    This monad is used to combine simplifications in a way that allows
+    to know if at least one simplification was performed, or not. *)
+
+type +'a t = 'a * [ `Same | `New]
+
+val return_same : 'a -> 'a t
+val return_new : 'a -> 'a t
+
+val return : 'a -> 'a t
+(** Alias to {!return_same} *)
+
+val get : 'a t -> 'a
+
+val is_new : _ t -> bool
+val is_same : _ t -> bool
+
+val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+(** Monadic bind *)
+
+val map : ('a -> 'b) -> 'a t -> 'b t
+
+val app_list : ('a -> 'a t) list -> 'a -> 'a t
+
+val map_l : ('a -> 'b t) -> 'a list -> 'b list t
+
+val fold_l : ('a -> 'b -> 'a t) -> 'a -> 'b list -> 'a t
+
+module Infix : sig
+  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+  val (>|=) : 'a t -> ('a -> 'b) -> 'b t
+end
