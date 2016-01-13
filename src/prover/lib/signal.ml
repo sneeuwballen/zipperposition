72313fd2db85c6aeb3346eee9ec893fcc4bd4bdc
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-01-13 19:38:11 +0100

    cleanup of Signal

--- src/prover/lib/signal.ml
+++ src/prover/lib/signal.ml
@@ -1,27 +1,5 @@
-(*
-Copyright (c) 2013, Simon Cruanes
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.  Redistributions in binary
-form must reproduce the above copyright notice, this list of conditions and the
-following disclaimer in the documentation and/or other materials provided with
-the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*)
+(* This file is free software, part of Zipperposition. See file "license" for more details. *)
 
 (** {1 Basic signal} *)
 
@@ -41,7 +19,7 @@ and keepalive =
 
 let _exn_handler = ref (fun _ -> ())
 
-let nop_handler x = ContinueListening
+let nop_handler _ = ContinueListening
 
 let create () =
   let s = {
@@ -55,7 +33,7 @@ let create () =
 let remove s i =
   assert (s.n > 0 && i >= 0);
   if i < s.n - 1  (* erase handler with the last one *)
-    then s.handlers.(i) <- s.handlers.(s.n - 1);
+  then s.handlers.(i) <- s.handlers.(s.n - 1);
   s.handlers.(s.n - 1) <- nop_handler; (* free handler *)
   s.n <- s.n - 1;
   ()
@@ -64,11 +42,11 @@ let send s x =
   for i = 0 to s.n - 1 do
     while
       begin try match s.handlers.(i) x with
-      | ContinueListening -> false  (* keep *)
-      | StopListening -> true
-      with e ->
-        !_exn_handler e;
-        false  (* be conservative, keep... *)
+        | ContinueListening -> false  (* keep *)
+        | StopListening -> true
+        with e ->
+          !_exn_handler e;
+          false  (* be conservative, keep... *)
       end
     do
       remove s i  (* i-th handler is done, remove it *)
@@ -78,19 +56,21 @@ let send s x =
 let on s f =
   (* resize handlers if needed *)
   if s.n = Array.length s.handlers
-    then begin
-      let handlers = Array.make (s.n + 4) nop_handler in
-      Array.blit s.handlers 0 handlers 0 s.n;
-      s.handlers <- handlers
-    end;
+  then begin
+    let handlers = Array.make (s.n + 4) nop_handler in
+    Array.blit s.handlers 0 handlers 0 s.n;
+    s.handlers <- handlers
+  end;
   s.handlers.(s.n) <- f;
   s.n <- s.n + 1
+
+let on_every s f =
+  on s (fun x -> ignore (f x); ContinueListening)
 
 let once s f =
   on s (fun x -> ignore (f x); StopListening)
 
-let propagate a b =
-  on a (fun x -> send b x; ContinueListening)
+let propagate a b = on_every a (send b)
 
 (** {2 Combinators} *)
 
@@ -100,9 +80,9 @@ let map signal f =
   let r = Weak.create 1 in
   Weak.set r 0 (Some signal');
   on signal (fun x ->
-    match Weak.get r 0 with
-    | None -> StopListening
-    | Some signal' -> send signal' (f x); ContinueListening);
+      match Weak.get r 0 with
+      | None -> StopListening
+      | Some signal' -> send signal' (f x); ContinueListening);
   signal'.alive <- Keep signal;
   signal'
 
@@ -112,9 +92,9 @@ let filter signal p =
   let r = Weak.create 1 in
   Weak.set r 0 (Some signal');
   on signal (fun x ->
-    match Weak.get r 0 with
-    | None -> StopListening
-    | Some signal' -> (if p x then send signal' x); ContinueListening);
+      match Weak.get r 0 with
+      | None -> StopListening
+      | Some signal' -> (if p x then send signal' x); ContinueListening);
   signal'.alive <- Keep signal;
   signal'
 
