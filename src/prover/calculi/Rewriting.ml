279db0dc505c0dd729bdbb184e52ff262d625f24
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-04-15 10:48:32 +0200

    implement narrowing on literals

--- src/prover/calculi/Rewriting.ml
+++ src/prover/calculi/Rewriting.ml
@@ -10,6 +10,9 @@ module RR = Rewrite_rule
 
 let section = RR.section
 
+let stat_narrowing_lit = Util.mk_stat "narrow.clause_steps"
+let stat_narrowing_term = Util.mk_stat "narrow.term_steps"
+
 module Make(E : Env_intf.S) = struct
   module Env = E
   module C = E.C
@@ -23,6 +26,8 @@ module Make(E : Env_intf.S) = struct
         "@[<2>@{<green>rewrite@} `@[%a@]`@ into `@[%a@]`@]" (fun k->k T.pp t T.pp t');
       Some t'
     )
+
+  (* TODO: term narrowing + contextual extended narrowing *)
 
   (* XXX: for now, we only do one step, and let Env.multi_simplify
      manage the fixpoint *)
@@ -42,7 +47,41 @@ module Make(E : Env_intf.S) = struct
           (fun k->k C.pp c CCFormat.(list C.pp) clauses);
         Some clauses
 
-  (* TODO: narrowing, on terms and clauses *)
+  (* narrowing on literals of given clause, using lits rewrite rules *)
+  let narrow_lits rules c =
+    let eligible = C.Eligible.res c in
+    let lits = C.lits c in
+    Literals.fold_lits ~eligible lits
+    |> Sequence.fold
+      (fun acc (lit,i) ->
+         RR.narrow_lit (rules,1) (lit,0)
+         |> Sequence.fold
+           (fun acc (rule,subst) ->
+              let proof =
+                ProofStep.mk_inference [C.proof c]
+                  ~rule:(ProofStep.mk_rule "narrow_clause") in
+              let lits' = CCArray.except_idx lits i in
+              (* create new clauses that correspond to replacing [lit]
+                 by [rule.rhs] *)
+              let clauses =
+                List.map
+                  (fun c' ->
+                     let renaming = E.Ctx.renaming_clear () in
+                     let new_lits =
+                       (Literal.apply_subst_list ~renaming subst (lits',0)) @
+                       (Literal.apply_subst_list ~renaming subst (c',1))
+                     in
+                     C.create ~trail:(C.trail c) new_lits proof)
+                  (RR.rhs_clause rule)
+              in
+              Util.debugf ~section 3
+                "@[<2>narrowing of `@[%a@]`@ using `@[%a@]`@ with @[%a@]@ yields @[%a@]@]"
+                (fun k->k C.pp c RR.pp_rule_clause rule Substs.pp subst
+                    CCFormat.(list (hovbox C.pp)) clauses);
+              Util.incr_stat stat_narrowing_lit;
+              List.rev_append clauses acc)
+           acc)
+      []
 
   let setup rules =
     Util.debug ~section 1 "register Rewriting to Env...";
@@ -51,6 +90,7 @@ module Make(E : Env_intf.S) = struct
       E.Ctx.lost_completeness ();
       E.add_rewrite_rule "rewrite_defs" (simpl_term rules);
       E.add_multi_simpl_rule (simpl_clause rules);
+      E.add_unary_inf "narrow_defs" (narrow_lits rules);
     )
 end
 
