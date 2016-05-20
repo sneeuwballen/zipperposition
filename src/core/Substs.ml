e303e57bc3016caa01de49b8af03f055a7544694
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-05-20 20:19:31 +0200

    small optim

--- src/core/Substs.ml
+++ src/core/Substs.ml
@@ -235,7 +235,9 @@ let apply subst ~renaming t =
   aux t
 
 let apply_no_renaming subst t =
-  apply subst ~renaming:Renaming.dummy t
+  if is_empty subst
+  then Scoped.get t
+  else apply subst ~renaming:Renaming.dummy t
 
 (** {2 Specializations} *)
 
