a7af7e179a1fac85a1914b55b603cdc8e2394d8d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-02-23 21:08:04 +0100

    [dev] add limit on the number of vars per clause in zipper

--- src/prover/calculi/heuristics.ml
+++ src/prover/calculi/heuristics.ml
@@ -9,12 +9,16 @@ module T = FOTerm
 module Lit = Literal
 
 let depth_limit_ = ref None
+let max_vars = ref 6
 
 let enable_depth_limit i =
   if i <= 0 then invalid_arg "Heuristics.enable_depth_limit";
   depth_limit_ := Some i
 
 let section = Util.Section.make ~parent:Const.section "heuristics"
+
+let stat_depth_limit = Util.mk_stat "trivial.too_deep"
+let stat_vars = Util.mk_stat "trivial.too_many_vars"
 
 (** {2 Rules} *)
 
@@ -48,14 +52,27 @@ module Make(E : Env.S) = struct
         if depth > d
         then (
           Ctx.lost_completeness();
+          Util.incr_stat stat_depth_limit;
           Util.debugf ~section 5 "@[<2>clause dismissed (too deep at %d):@ @[%a@]@]"
             (fun k->k depth C.pp c);
           true
         ) else false
 
+  let has_too_many_vars c =
+    let lits = C.lits c in
+    let n_vars = Literals.vars lits |> List.length in
+    if n_vars > !max_vars then (
+      Ctx.lost_completeness();
+      Util.incr_stat stat_vars;
+      Util.debugf ~section 5 "@[<2>clause dismissed (%d vars is too much):@ @[%a@]@]"
+        (fun k->k n_vars C.pp c);
+      true
+    ) else false
+
   let register () =
     Util.debug ~section 2 "register heuristics...";
     Env.add_is_trivial is_too_deep;
+    Env.add_is_trivial has_too_many_vars;
     ()
 end
 
@@ -71,9 +88,8 @@ let extension =
 
 let () =
   Params.add_opts
-    [ "--depth-limit"
-    , Arg.Int enable_depth_limit
-    , " set maximal term depth"
+    [ "--depth-limit", Arg.Int enable_depth_limit, " set maximal term depth";
+      "--max-vars", Arg.Set_int max_vars, " maximum number of variables per clause";
     ];
   ()
 
