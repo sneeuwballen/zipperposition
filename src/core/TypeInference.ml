9f809a2b4b86035a7b7da8fa6f27c976cb6f54b2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-05-09 18:50:20 +0200

    bugfix in type inference

--- src/core/TypeInference.ml
+++ src/core/TypeInference.ml
@@ -563,9 +563,9 @@ let rec infer_rec ?loc ctx t =
       then error_ ?loc "(in)equation @[%a@] ?= @[%a@] between types is forbidden" T.pp a T.pp b;
       begin match conn with
         | Builtin.Eq ->
-          if T.Ty.returns_prop (T.ty_exn a) then T.Form.equiv a b else T.Form.eq a b
+          if T.Ty.is_prop (T.ty_exn a) then T.Form.equiv a b else T.Form.eq a b
         | Builtin.Neq ->
-          if T.Ty.returns_prop (T.ty_exn a) then T.Form.xor a b else T.Form.neq a b
+          if T.Ty.is_prop (T.ty_exn a) then T.Form.xor a b else T.Form.neq a b
         | _ -> assert false
       end
     | PT.Bind(((Binder.Forall | Binder.Exists) as binder), vars, f') ->
