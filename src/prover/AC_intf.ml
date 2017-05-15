a7dfca2a439b77a57abd5f90fe51e96229f57641
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-05-15 20:38:33 +0200

    refactor proof handling in AC

--- src/prover/AC_intf.ml
+++ src/prover/AC_intf.ml
@@ -14,13 +14,13 @@ module type S = sig
 
   val on_add : spec Signal.t
 
-  val add : ?proof:Proof.t list -> ID.t -> Type.t -> unit
+  val add : proof:Proof.step -> ID.t -> Type.t -> unit
   (** Declare that the given symbol is AC, and update the Env subsequently
       by adding clauses, etc. *)
 
   val is_ac : ID.t -> bool
 
-  val find_proof : ID.t -> Proof.t list
+  val find_proof : ID.t -> Proof.step
   (** Recover the proof for the AC-property of this symbol.
       @raise Not_found if the symbol is not AC *)
 
@@ -30,18 +30,10 @@ module type S = sig
   val symbols_of_terms : Term.t Sequence.t -> ID.Set.t
   (** set of AC symbols occurring in the given term *)
 
-  val proofs : unit -> Proof.t list
-  (** All proofs for all AC axioms *)
-
   val exists_ac : unit -> bool
   (** Is there any AC symbol? *)
 
-  val axioms : ID.t -> Type.t -> C.t list
-  (** List of (persistent) axioms that are needed for simplifications to
-      be complete for the given symbol. The [ctx] is required for type inference
-      and building clauses . *)
-
-  val scan_statement : (_, Term.t, Type.t) Statement.t -> unit
+  val scan_statement : Statement.clause_t -> unit
   (** Check whether the statement contains an "AC" attribute, do the proper
       declaration in this case *)
 
