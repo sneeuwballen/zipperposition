3ce310f255624d985e408162a7bd3a03dabd15fd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-05-17 12:17:33 +0200

    bugfix in `Int_lit` (normalization of divisibility literals)

--- src/core/Int_lit.ml
+++ src/core/Int_lit.ml
@@ -148,6 +148,105 @@ let mk_neq = make Different
 let mk_less = make Less
 let mk_lesseq = make Lesseq
 
+module U = struct
+  module ZTbl = Hashtbl.Make(Z)
+
+  type divisor = {
+    prime : Z.t;
+    power : int;
+  }
+
+  let two = Z.of_int 2
+
+  (* table from numbers to some of their divisor (if any) *)
+  let _table = lazy (
+    let t = ZTbl.create 256 in
+    ZTbl.add t two None;
+    t)
+
+  let _divisors n = ZTbl.find (Lazy.force _table) n
+
+  let _add_prime n =
+    ZTbl.replace (Lazy.force _table) n None
+
+  (* add to the table the fact that [d] is a divisor of [n] *)
+  let _add_divisor n d =
+    assert (not (ZTbl.mem (Lazy.force _table) n));
+    ZTbl.add (Lazy.force _table) n (Some d)
+
+  (* primality test, modifies _table *)
+  let _is_prime n0 =
+    let n = ref two in
+    let bound = Z.succ (Z.sqrt n0) in
+    let is_prime = ref true in
+    while !is_prime && Z.leq !n bound do
+      if Z.sign (Z.rem n0 !n) = 0
+      then begin
+        is_prime := false;
+        _add_divisor n0 !n;
+      end;
+      n := Z.succ !n;
+    done;
+    if !is_prime then _add_prime n0;
+    !is_prime
+
+  let is_prime n =
+    try
+      begin match _divisors n with
+        | None -> true
+        | Some _ -> false
+      end
+    with Not_found ->
+    match Z.probab_prime n 7 with
+      | 0 -> false
+      | 2 -> (_add_prime n; true)
+      | 1 ->
+        _is_prime n
+      | _ -> assert false
+
+  let rec _merge l1 l2 = match l1, l2 with
+    | [], _ -> l2
+    | _, [] -> l1
+    | p1::l1', p2::l2' ->
+      match Z.compare p1.prime p2.prime with
+        | 0 ->
+          {prime=p1.prime; power=p1.power+p2.power} :: _merge l1' l2'
+        | n when n < 0 ->
+          p1 :: _merge l1' l2
+        | _ -> p2 :: _merge l1 l2'
+
+  let rec _decompose n =
+    try
+      begin match _divisors n with
+        | None -> [{prime=n; power=1;}]
+        | Some q1 ->
+          let q2 = Z.divexact n q1 in
+          _merge (_decompose q1) (_decompose q2)
+      end
+    with Not_found ->
+      ignore (_is_prime n);
+      _decompose n
+
+  let prime_decomposition n =
+    if is_prime n
+    then [{prime=n; power=1;}]
+    else _decompose n
+
+  let primes_leq n0 k =
+    let n = ref two in
+    while Z.leq !n n0 do
+      if is_prime !n then k !n
+    done
+end
+
+(* reduce 8^1 to 2^3 *)
+let _normalize_n n power =
+  let divisors = U.prime_decomposition (Z.pow n power) in
+  match divisors with
+    | [] -> assert false
+    | [d] -> d.U.prime, d.U.power
+    | _::_::_ -> n, power
+
 (* normalize coefficients of [m] in Z/n^power Z *)
 let _normalize_in_div n ~power m =
   let nk = Z.pow n power in
@@ -157,6 +256,7 @@ let _normalize_in_div n ~power m =
 
 let mk_divides ?(sign=true) n ~power m =
   let m = Monome.normalize m in
+  let n, power = _normalize_n n power in
   let m = _normalize_in_div n ~power m in
   (* factorize m by some k; if k is n^p, then make the literal
      n^{power-p} | m/k *)
@@ -889,93 +989,4 @@ module Focus = struct
   let to_string = CCFormat.to_string pp
 end
 
-module Util = struct
-  module ZTbl = Hashtbl.Make(Z)
-
-  type divisor = {
-    prime : Z.t;
-    power : int;
-  }
-
-  let two = Z.of_int 2
-
-  (* table from numbers to some of their divisor (if any) *)
-  let _table = lazy (
-    let t = ZTbl.create 256 in
-    ZTbl.add t two None;
-    t)
-
-  let _divisors n = ZTbl.find (Lazy.force _table) n
-
-  let _add_prime n =
-    ZTbl.replace (Lazy.force _table) n None
-
-  (* add to the table the fact that [d] is a divisor of [n] *)
-  let _add_divisor n d =
-    assert (not (ZTbl.mem (Lazy.force _table) n));
-    ZTbl.add (Lazy.force _table) n (Some d)
-
-  (* primality test, modifies _table *)
-  let _is_prime n0 =
-    let n = ref two in
-    let bound = Z.succ (Z.sqrt n0) in
-    let is_prime = ref true in
-    while !is_prime && Z.leq !n bound do
-      if Z.sign (Z.rem n0 !n) = 0
-      then begin
-        is_prime := false;
-        _add_divisor n0 !n;
-      end;
-      n := Z.succ !n;
-    done;
-    if !is_prime then _add_prime n0;
-    !is_prime
-
-  let is_prime n =
-    try
-      begin match _divisors n with
-        | None -> true
-        | Some _ -> false
-      end
-    with Not_found ->
-    match Z.probab_prime n 7 with
-      | 0 -> false
-      | 2 -> (_add_prime n; true)
-      | 1 ->
-        _is_prime n
-      | _ -> assert false
-
-  let rec _merge l1 l2 = match l1, l2 with
-    | [], _ -> l2
-    | _, [] -> l1
-    | p1::l1', p2::l2' ->
-      match Z.compare p1.prime p2.prime with
-        | 0 ->
-          {prime=p1.prime; power=p1.power+p2.power} :: _merge l1' l2'
-        | n when n < 0 ->
-          p1 :: _merge l1' l2
-        | _ -> p2 :: _merge l1 l2'
-
-  let rec _decompose n =
-    try
-      begin match _divisors n with
-        | None -> [{prime=n; power=1;}]
-        | Some q1 ->
-          let q2 = Z.divexact n q1 in
-          _merge (_decompose q1) (_decompose q2)
-      end
-    with Not_found ->
-      ignore (_is_prime n);
-      _decompose n
-
-  let prime_decomposition n =
-    if is_prime n
-    then [{prime=n; power=1;}]
-    else _decompose n
-
-  let primes_leq n0 k =
-    let n = ref two in
-    while Z.leq !n n0 do
-      if is_prime !n then k !n
-    done
-end
+module Util = U
