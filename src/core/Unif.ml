785f868d46d68dc9d847562350bcf1828c649c69
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-07-11 14:41:47 +0200

    bugfix: do occur check for same-scope matching

--- src/core/Unif.ml
+++ src/core/Unif.ml
@@ -199,10 +199,10 @@ module Inner = struct
             fail() (* blocked variable *)
           | T.Var _, _, O_match_protect (P_scope sc) when sc1 = sc ->
             fail() (* variable belongs to the protected scope *)
-          | T.Var v1, _, O_match_protect _ ->
-            (* no need for occur check when matching *)
+          | T.Var v1, _, O_match_protect (P_scope _) ->
+            (* no need for occur check when matching from distinct scopes *)
             US.bind subst (v1,sc1) (t2,sc2)
-          | T.Var v1, _, O_unify ->
+          | T.Var v1, _, (O_unify | O_match_protect (P_vars _)) ->
             if occurs_check ~depth:0 (US.subst subst) (v1,sc1) (t2,sc2)
             then fail () (* occur check or t2 is open *)
             else US.bind subst (v1,sc1) (t2,sc2)
