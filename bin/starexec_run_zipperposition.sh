6e4ece2fb333f41e09361652075ef0ec4ae743f9
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-07-13 13:11:37 +0200

    more files

---
+++ bin/starexec_run_zipperposition.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ 
+export LD=./ld-2.25.so
+export LD_LIBRARY_PATH=.
+
+"$LD" ./zipperposition.native -o tptp "$1" \
+  --timeout "$STAREXEC_WALLCLOCK_LIMIT" \
+  --mem-limit "$STAREXEC_MAX_MEM"
