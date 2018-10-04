842443b726704827da703a22bb15c746a4660eb7
Alexander Bentkamp <a.bentkamp@vu.nl>
2018-10-04 14:40:00 +0200

    JP Unif: new substitutions and polymorphism

--- src/core/JP_unif.ml
+++ src/core/JP_unif.ml
@@ -4,11 +4,34 @@
 (** {1 Jensen-Pietrzykowski Unification} *)
 
 module T = Term
-module S = Subst
 
 let scope = 0 (* TODO: scopes *)
 
-let subst_bind s (v:T.var) t = ((Subst.FO.bind s ((v,scope):>InnerTerm.t HVar.t Scoped.t)) (t,scope)) 
+type subst = Subst.t Sequence.t
+
+module S = struct
+
+  let empty = Sequence.empty
+  
+  let add s (v:T.var) t = Sequence.snoc s ((Subst.FO.bind Subst.empty ((v,scope):>InnerTerm.t HVar.t Scoped.t)) (t,scope)) 
+
+  let append = Sequence.append
+
+  let singleton (v:T.var) t = add empty v t
+
+  let of_subst s = Sequence.singleton s
+
+  let apply s t = s |> Sequence.fold (fun term subst -> Subst.FO.apply Subst.Renaming.none subst (term, scope)) t
+
+  let apply_ty s ty = s |> Sequence.fold (fun term subst -> Subst.Ty.apply Subst.Renaming.none subst (ty, scope)) ty
+
+end
+
+let unif_ty t s = 
+  try 
+    let type_unifier = S.of_subst (Unif.Ty.unify_syn ~subst:Subst.empty (t, scope) (s, scope)) in
+    Some type_unifier
+  with Unif.Fail -> None
 
 (** {1 Projection rule} *)
 
@@ -21,10 +44,15 @@ let project_onesided u =
     args 
     |> OSeq.of_list
     |> OSeq.mapi (fun i arg -> List.length prefix_types - 1 - i, arg) (* Determine DB-index of the argument *)
-    |> OSeq.filter (fun (_, arg) -> T.ty arg = return_type) (* necessary to make substitution type correct *)
-    |> OSeq.map (fun (dbindex, arg) -> 
+    |> OSeq.filter_map (fun (dbindex, arg) -> 
+      (* Unify type of argument and return type *)
+      match unif_ty (T.ty arg) (return_type) with
+        | Some type_unifier -> Some (dbindex, arg, type_unifier)
+        | None -> None
+    )
+    |> OSeq.map (fun (dbindex, arg, type_unifier) -> 
         (* substitute x for a projection to the j-th argument *)
-        subst_bind Subst.empty (T.as_var_exn head) (T.fun_l prefix_types (T.bvar ~ty:(T.ty arg) dbindex))
+        S.add type_unifier (T.as_var_exn head) (T.fun_l prefix_types (T.bvar ~ty:(T.ty arg) dbindex))
     )
   else OSeq.empty
 
@@ -52,7 +80,7 @@ let imitate_onesided u v =
     in
     let matrix = T.app head_v matrix_args in
     let subst_value = T.fun_l prefix_types_u matrix in 
-    let subst = subst_bind Subst.empty (T.as_var_exn head_u) subst_value in
+    let subst = S.singleton (T.as_var_exn head_u) subst_value in
     OSeq.return subst
   else OSeq.empty
 
@@ -93,9 +121,8 @@ let identify u v (_ : (T.var * int) list) =
     let matrix_v = T.app matrix_head (matrix_args_v @ bvars_v) in
     let subst_value_u = T.fun_l prefix_types_u matrix_u in 
     let subst_value_v = T.fun_l prefix_types_v matrix_v in 
-    let subst = Subst.empty in
-    let subst = subst_bind subst (T.as_var_exn head_u) subst_value_u in
-    let subst = subst_bind subst (T.as_var_exn head_v) subst_value_v in
+    let subst = S.singleton (T.as_var_exn head_u) subst_value_u in
+    let subst = S.add subst (T.as_var_exn head_v) subst_value_v in
     OSeq.return subst
   else OSeq.empty
 
@@ -112,7 +139,7 @@ let eliminate _ _ l =
     let matrix_head = T.var (HVar.fresh ~ty:(Type.arrow prefix_types' return_type) ()) in
     let matrix = T.app matrix_head bvars' in
     let subst_value = T.fun_l prefix_types matrix in
-    let subst = subst_bind Subst.empty v subst_value in
+    let subst = S.singleton v subst_value in
     subst
   )
   |> OSeq.of_list
@@ -138,8 +165,8 @@ let iterate_one types_w v prefix_types return_type i type_ul =
   let var_x = T.var (HVar.fresh ~ty:(Type.arrow (prefix_types @ [Type.arrow types_w return_type_ul]) return_type) ()) in
   let matrix = T.app var_x (bvars_u @ [inner_lambda_expr]) in
   let subst_value = T.fun_l prefix_types matrix in
-  let subst = subst_bind Subst.empty v subst_value in
-  subst
+  subst_value
+
 
 let iterate u v l =
   (* The variable can be either above the disagreement pair (i.e., in l) 
@@ -156,7 +183,7 @@ let iterate u v l =
         prefix_types 
         |> List.mapi
           (fun i type_ul ->
-            if Type.is_fun type_ul
+            if Type.is_fun type_ul || Type.is_var type_ul
             then 
               Some (v, prefix_types, return_type, i, type_ul)
             else 
@@ -177,7 +204,21 @@ let iterate u v l =
         positions
         |> OSeq.map
           (fun (v, prefix_types, return_type, i, type_ul) -> 
-            Some (iterate_one types_w v prefix_types return_type i type_ul)
+            if Type.is_fun type_ul 
+            then Some (S.singleton v (iterate_one types_w v prefix_types return_type i type_ul))
+            else 
+              (* To get a complete polymorphic algorithm, we need to consider the case that a type variable could be instantiated as a function. *)
+              match Type.view type_ul with
+                | Type.Var alpha -> 
+                  let beta = (HVar.fresh ~ty:Type.tType ()) in
+                  let gamma = (HVar.fresh ~ty:Type.tType ()) in
+                  let alpha' = (Type.arrow [Type.var beta] (Type.var gamma)) in
+                  let ty_subst = S.singleton alpha (Term.of_ty alpha') in
+                  let v' = HVar.cast ~ty:(S.apply_ty ty_subst (HVar.ty v)) v in
+                  let prefix_types' = prefix_types |> CCList.map (S.apply_ty ty_subst) in
+                  let return_type' = S.apply_ty ty_subst return_type in
+                  Some (S.add ty_subst v' (iterate_one types_w v' prefix_types' return_type' i alpha'))
+                | _ -> None
           )
         (* Append some "None"s to delay the substitutions containing long w tuples *)
         |> (fun seq -> OSeq.append seq (OSeq.take 50 (OSeq.repeat None)))
@@ -189,9 +230,9 @@ let iterate u v l =
 (** {6 Unification procedure} *)
 
 (* apply a substitution and reduce to normal form *)
-let nfapply s u = Lambda.eta_expand (Lambda.snf (S.FO.apply S.Renaming.none s (u, scope)))
+let nfapply s u = Lambda.eta_expand (Lambda.snf (S.apply s u))
 
-(* TODO: comparison form is actually slightly different from short normal form! *)
+(* TODO: comparison form is actually slightly different, but eta_expand also works *)
 
 (* Find disagreeing subterms. 
     This function also returns a list of variables occurring above the
@@ -199,33 +240,45 @@ let nfapply s u = Lambda.eta_expand (Lambda.snf (S.FO.apply S.Renaming.none s (u
     pair occurs in. 
     TODO: preferably one that is not below a variable (to get preunification if possible) 
 *)
-let rec find_disagreement s t = 
+let find_disagreement s t = 
   let rec find_disagreement_l ?(applied_var = None) ?(argindex=0) ss tt = 
     match ss, tt with
-      | [], [] -> None
+      | [], [] -> OSeq.empty
       | s' :: ss', t' :: tt' -> 
-        let d = find_disagreement s' t' in 
-        begin match d with
-        | Some ((u,v),l) -> 
-          begin match applied_var with
-            | Some x -> Some ((u,v), (T.as_var_exn x, argindex) :: l) 
-            | None -> d
-          end
-        | None -> 
-            let argindex = argindex + 1 in
-            find_disagreement_l ~applied_var ~argindex ss' tt'
-        end
+        find_disagreement_aux s' t'
+        |> OSeq.map (fun ((u,v),l) ->
+            begin match applied_var with
+              | Some x -> ((u,v), (T.as_var_exn x, argindex) :: l) 
+              | None -> ((u,v),l)
+            end
+        )
+        |> (fun s -> OSeq.append s (find_disagreement_l ~applied_var ~argindex:(argindex + 1) ss' tt'))
       | _, _ -> raise (Invalid_argument "types of unified terms should be equal")
+  and find_disagreement_aux s t = 
+    match T.view s, T.view t with
+      | T.App (f, ss), T.App (g, tt) when f = g && not (T.is_var f) -> 
+        find_disagreement_l ss tt 
+      | T.App (f, ss), T.App (g, tt) when f = g && T.is_var f -> 
+        find_disagreement_l ~applied_var:(Some f) ss tt 
+      | T.AppBuiltin (f, ss), T.AppBuiltin (g, tt) when f = g -> 
+        find_disagreement_l ss tt 
+      | T.Var x, T.Var y when x = y -> OSeq.empty
+      | T.DB i, T.DB j when i = j -> OSeq.empty
+      | T.Const a, T.Const b when a = b -> OSeq.empty
+      | T.Fun (ty_s, s'), T.Fun (ty_t, t') -> 
+        assert (ty_s == ty_t); 
+        find_disagreement_aux s' t'
+      | _ -> OSeq.return ((s, t),[])
   in
-  match T.view s, T.view t with
-    | T.App (f, ss), T.App (g, tt) when f = g && not (T.is_var f)-> find_disagreement_l ss tt 
-    | T.App (f, ss), T.App (g, tt) when f = g && T.is_var f -> find_disagreement_l ~applied_var:(Some f) ss tt 
-    | T.AppBuiltin (f, ss), T.AppBuiltin (g, tt) when f = g -> find_disagreement_l ss tt 
-    | T.Var x, T.Var y when x = y -> None
-    | T.DB i, T.DB j when i = j -> None
-    | T.Const a, T.Const b when a = b -> None
-    | T.Fun (_, s'), T.Fun (_, t') -> find_disagreement s' t' (*TODO: what about the types?*)
-    | _ -> Some ((s, t),[])
+  let s = find_disagreement_aux s t in
+  match OSeq.find (fun ((u,v),l) -> CCList.is_empty l) s with
+    | Some d -> Some d
+    | None -> 
+      begin match s () with
+        | OSeq.Nil -> None
+        | OSeq.Cons (d, _) -> Some d
+      end
+
 
 (* TODO: Replace by the updated OSeq.merge *)
 (** Dovetailing through a sequence of sequences:
@@ -277,25 +330,25 @@ let unify t s =
               let s_subst = nfapply subst s in
               let unifiers = unify_terms t_subst s_subst ~rules:(rules @ [rulename]) in
               unifiers 
-              |> OSeq.map (CCOpt.map (fun unifier -> Subst.merge unifier subst))
-              (* We actually want to concatenate, but Subst.merge should do the job 
-                since the substitutes only contain fresh variables *)
+              |> OSeq.map (CCOpt.map (fun unifier -> S.append subst unifier))
             | None -> OSeq.empty
           )
         |> dovetail 
         |> OSeq.append (OSeq.return None)
       | None -> 
-        (* Util.debugf 1 "@[...unified!@ @[(rules: %a)@]@]" (fun k -> k (CCList.pp CCString.pp) rules); *)
-        OSeq.return (Some Subst.empty)
+        assert (t == s);
+        Util.debugf 1 "@[...unified!@ @[(rules: %a)@]@]" (fun k -> k (CCList.pp CCString.pp) rules); 
+        OSeq.return (Some S.empty)
   in
   (* Unify types first ... *)
-  let type_unifier = Subst.empty in
-  let type_unifier = Unif.Ty.unify_syn ~subst:type_unifier (T.ty t, 0) (T.ty s, 0) in
-  let t' = nfapply type_unifier t in
-  let s' = nfapply type_unifier s in
-  (* ... then terms. *)
-  let term_unifiers = unify_terms t' s' ~rules:[] in
-  OSeq.map (CCOpt.map (Subst.merge type_unifier)) term_unifiers
+  match unif_ty (T.ty t) (T.ty s) with
+    | Some type_unifier ->
+      let t' = nfapply type_unifier t in
+      let s' = nfapply type_unifier s in
+      (* ... then terms. *)
+      let term_unifiers = unify_terms t' s' ~rules:[] in
+      OSeq.map (CCOpt.map (S.append type_unifier)) term_unifiers
+    | None -> OSeq.empty
 
 (* TODO: Remove tracking of rules for efficiency? *)
 
