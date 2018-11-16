47c34de082897e8ce90688417806009989883e26
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2018-11-16 15:56:56 +0100

    add stream data structure

---
+++ src/prover/stream.ml
@@ -0,0 +1,53 @@
+
+(** {1 Stream}*)
+
+open Logtk
+
+let stat_stream_create = Util.mk_stat "stream.create"
+
+(** {2 Signature} *)
+module type S = Stream_intf.S
+
+module Make(X : sig
+    module Ctx : Ctx.S
+  end)
+  : S with module Ctx = X.Ctx
+= struct
+  module Ctx = X.Ctx
+  module C = Clause.Make(Ctx)
+
+
+type t = {
+  id : int; (** unique ID of the stream *)
+  penalty: int; (** heuristic penalty *)
+  mutable stm : C.t option OSeq.t; (** the stream itself *)
+}
+
+let id_count_ = ref 0
+
+(** {2 Basics} *)
+
+let make ?penalty:(p=1) s =
+  Util.incr_stat stat_stream_create;
+  let id = !id_count_ in
+  incr id_count_;
+  { id; penalty = p; stm = s; }
+
+let equal s1 s2 = s1.id = s2.id
+let compare s1 s2 = Pervasives.compare s1.id s2.id
+let id s = s.id
+let hash s = Hashtbl.hash s.id
+
+(* TODO: does it really pop an element? *)
+(* normally it should be fine, check with Simon *)
+let is_empty s = OSeq.is_empty s.stm
+
+(* No length function because some streams are infinite *)
+
+let penalty s = s.penalty
+
+let pp out s =
+  Format.fprintf out "stream %i" s.id;
+  ()
+
+end
