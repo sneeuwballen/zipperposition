094a509238cbef4b02132abe7e739cb0b1a5af5a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-10-04 18:52:50 -0500

    feat(llprover): add extensionality axioms to theory of lambdas

--- src/proofs/LLProver.ml
+++ src/proofs/LLProver.ml
@@ -97,7 +97,7 @@ let can_check : LLProof.tag list -> bool =
   let f = function
     | T_ho | T_ext | T_defexp -> true
     | T_lra | T_lia | T_ind | T_data
-    | T_distinct | T_ac _ 
+    | T_distinct | T_ac _
     | T_conv | T_avatar -> false
   in
   List.for_all f
@@ -171,26 +171,49 @@ module Th_lambda = struct
   module N = SI.CC.N
   module Expl = SI.CC.Expl
   module N_tbl = Sidekick_util.Backtrackable_tbl.Make(N)
+  module T_tbl = Sidekick_util.Backtrackable_tbl.Make(T)
+  module T2_tbl = CCHashtbl.Make(struct
+      type t = T.t * T.t
+      let equal = CCPair.equal T.equal T.equal
+      let hash = CCHash.pair T.hash T.hash
+    end)
 
   (* a node [lm_node] decorated with a lambda-term *)
   type lambda_node = {
     lm_node: N.t;
+    lm_ty_arg: T.t;
     lm_body: T.t;
+  }
+  type ext_skolem = {
+    sko: T.t;
+    mutable instantiated: bool;
   }
 
   type state = {
-    lambdas: lambda_node list N_tbl.t; (* repr -> lambdas for the class *)
+    lambdas_in_cls: lambda_node list N_tbl.t; (* repr -> lambdas_in_cls for the class *)
+    lambdas_of_ty: lambda_node list T_tbl.t; (* ty -> list of lambdas_in_cls of this type *)
+    ext_skolems: ext_skolem T2_tbl.t; (* pair of lambdas -> extensional skolem for this pair *)
   }
 
   let create tst : state =
-    { lambdas=N_tbl.create ~size:128 (); }
+    { lambdas_in_cls=N_tbl.create ~size:128 ();
+      lambdas_of_ty=T_tbl.create ~size:24 ();
+      ext_skolems=T2_tbl.create 24;
+    }
 
-  let push_level st = N_tbl.push_level st.lambdas
-  let pop_levels st n = N_tbl.pop_levels st.lambdas n
+  let push_level st =
+    N_tbl.push_level st.lambdas_in_cls;
+    T_tbl.push_level st.lambdas_of_ty;
+    ()
 
-  let get_lambdas st (n:N.t) : _ list =
-    N_tbl.get st.lambdas n |> CCOpt.get_or ~default:[]
-  
+  let pop_levels st n =
+    N_tbl.pop_levels st.lambdas_in_cls n;
+    T_tbl.pop_levels st.lambdas_of_ty n;
+    ()
+
+  let get_lambdas_in_cls st (n:N.t) : _ list =
+    N_tbl.get st.lambdas_in_cls n |> CCOpt.get_or ~default:[]
+
   let errorf msg = Util.errorf ~where:"llprover" msg
 
   (* do static beta-reduction *)
@@ -204,7 +227,7 @@ module Th_lambda = struct
             | Some ty_arg when T.equal ty_var ty_arg ->
               (* β-reduction *)
               let reduced_term = T.db_eval ~sub:arg body in
-              Util.debugf 3 ~section "@[th-lambda.beta-reduce@ :term %a@ :into %a@]" 
+              Util.debugf 3 ~section "@[th-lambda.beta-reduce@ :term %a@ :into %a@]"
                 (fun k -> k T.pp t T.pp reduced_term);
               let reduced_node = SI.CC.add_term cc reduced_term in
               let expl = SI.CC.Expl.mk_list [] in (* trivial *)
@@ -217,17 +240,17 @@ module Th_lambda = struct
       end
     | _ -> ()
 
-(* when merging classes [a] and [b], look in each class.
-   If the class [a] contains [λx. t], look in all parents of [b]
-   for some [apply b' u] where [b=b'].
-   For each such parent of [b], do the merge
-   [a=λx. t && b=b' && expl ==> apply b' u = t[x\u]] *)
+  (* when merging classes [a] and [b], look in each class.
+     If the class [a] contains [λx. t], look in all parents of [b]
+     for some [apply b' u] where [b=b'].
+     For each such parent of [b], do the merge
+     [a=λx. t && b=b' && expl ==> apply b' u = t[x\u]] *)
   let cc_on_pre_merge si (st:state)
       (cc:SI.CC.t) ac (a:N.t) (b:N.t) (expl_a_b:SI.CC.Expl.t) : unit =
-    let iter_lambdas a b =
-      match get_lambdas st a with
-      | [] -> () (* no lambdas *)
-      | lambdas ->
+    let iter_lambdas_in_cls a b =
+      match get_lambdas_in_cls st a with
+      | [] -> () (* no lambdas_in_cls *)
+      | lambdas_in_cls ->
         let app_parents =
           N.iter_parents b
           |> Iter.filter_map
@@ -242,7 +265,7 @@ module Th_lambda = struct
           app_parents
           |> Iter.flat_map
             (fun (n_parent_b, f, arg) ->
-               Iter.of_list lambdas |> Iter.map (fun lm -> n_parent_b, f, arg, lm))
+               Iter.of_list lambdas_in_cls |> Iter.map (fun lm -> n_parent_b, f, arg, lm))
         in
         all_new_beta
           (fun (n_parent_b, f, arg, lm) ->
@@ -250,8 +273,8 @@ module Th_lambda = struct
              (* [app f arg = body[x\arg]] because [f=b] and [b=a=λx. body] *)
              let new_t = T.db_eval ~sub:arg lm_body in
              Util.debugf 3 ~section
-               "(@[th-lambda.cc-beta-reduce@ :n1 %a@ :n2 %a@ \
-                :lambda-n1 %a@ :parent-n2 %a@ :new-t %a@])" 
+               "(@[th-lambda.cc-beta-reduce@ (merging n1=n2, n2 being applied)@ \
+                :n1 %a@ :n2 %a@ :lambda-n1 %a@ :parent-n2 %a@ :new-t %a@])"
                (fun k -> k N.pp a N.pp b N.pp lm_node N.pp n_parent_b T.pp new_t);
              let expl =
                Expl.mk_list
@@ -260,23 +283,85 @@ module Th_lambda = struct
              CC.merge_t cc (N.term n_parent_b) new_t expl;
           );
     in
-    iter_lambdas a b;
-    iter_lambdas b a;
-    let lms = get_lambdas st a @ get_lambdas st b in
+    iter_lambdas_in_cls a b;
+    iter_lambdas_in_cls b a;
+    let lms = get_lambdas_in_cls st a @ get_lambdas_in_cls st b in
     if lms <> [] then (
-      N_tbl.add st.lambdas a lms; (* update with the merge *)
+      N_tbl.add st.lambdas_in_cls a lms; (* update with the merge *)
     );
     ()
 
-  (* if [t] is a lambda term, add its node to the set of lambdas *)
+  let sorted_pair t1 t2 : T.t * T.t =
+    if T.compare t1 t2 <= 0 then t1, t2 else t2, t1
+
+  (* final check.
+     - extensionality:
+       add [lm=lm' => lm sko=lm' sko] for each pair of lambdas [lm, lm']
+       with the same type. [sko] is unique to the pair [lm,lm'].
+  *)
+  let final_check (st:state) (si:SI.t) (acts:SI.actions) (_trail:_) : unit =
+    let mk_lit ?sign t = SI.mk_lit si acts t in
+    let lambdas =
+      T_tbl.to_iter st.lambdas_of_ty
+      |> Iter.map snd
+      |> Iter.flat_map
+        (function
+          | [] -> assert false
+          | [_] -> Iter.empty (* no pairs *)
+          | l -> Iter.diagonal_l l)
+    in
+    begin
+      lambdas
+        (fun (lm1,lm2) ->
+           let t1 = N.term lm1.lm_node in
+           let t2 = N.term lm2.lm_node in
+           assert (not (T.equal t1 t2));
+           let pair = sorted_pair t1 t2 in
+           let sko =
+             try T2_tbl.find st.ext_skolems pair
+             with Not_found ->
+               let sko =
+                 let c = ID.makef "_ext_sko_%d" (T2_tbl.length st.ext_skolems) in
+                 T.const ~ty:lm1.lm_ty_arg c
+               in
+               let sko = {sko; instantiated=false} in
+               T2_tbl.add st.ext_skolems pair sko; (* save it *)
+               sko
+           in
+           if not sko.instantiated then (
+             sko.instantiated <- true;
+             (* axiom: [t1 sko=t2 sko => t1=t2] *)
+             let ext_axiom = [
+               mk_lit @@ T.Form.neq (T.app t1 sko.sko) (T.app t2 sko.sko);
+               mk_lit @@ T.Form.eq t1 t2;
+             ] in
+             Util.debugf ~section 5 "(@[th-lambda.add-ext-axiom@ %a@])"
+               (fun k->k (Fmt.Dump.list SI.Lit.pp) ext_axiom);
+             SI.add_clause_permanent si acts ext_axiom;
+           );
+           ())
+    end;
+    CC.check (SI.cc si) acts;
+    Util.debugf 5 ~section "(@[th-lambda.final-check@ :classes (@[%a@])@])"
+      (fun k->k (Util.pp_seq (Fmt.Dump.list N.pp))
+          (CC.all_classes (SI.cc si)
+           |> Iter.map (fun n-> N.iter_class n |> Iter.to_rev_list)));
+    ()
+
+  (* if [t] is a lambda term, add its node to the set of lambdas_in_cls *)
   let add_lambda (st:state) (n:SI.CC.N.t) (t:T.t) : unit =
     match T.view t with
-    | T.Bind {binder=Binder.Lambda; ty_var=_; body} ->
-      let lm = { lm_node=n; lm_body=body; } in
-      N_tbl.add st.lambdas n [lm];
+    | T.Bind {binder=Binder.Lambda; ty_var; body} ->
+      let lm = { lm_node=n; lm_body=body; lm_ty_arg=ty_var } in
+      N_tbl.add st.lambdas_in_cls n [lm];
+      let ty_t = T.ty_exn t in
+      let others =
+        try T_tbl.find st.lambdas_of_ty ty_t with Not_found -> []
+      in
+      T_tbl.add st.lambdas_of_ty ty_t (lm :: others);
     | _ -> ()
 
-  let cc_on_new_term _ (st:state) (cc:SI.CC.t) (n:SI.CC.N.t) (t:T.t) = 
+  let cc_on_new_term _ (st:state) (cc:SI.CC.t) (n:SI.CC.N.t) (t:T.t) =
     add_lambda st n t;
     beta_reduce st cc n;
     ()
@@ -284,6 +369,7 @@ module Th_lambda = struct
   let create_and_setup si =
     Util.debug 3 ~section "Setting up theory of lambda expressions.";
     let st = create (SI.tst si) in
+    SI.on_final_check si (final_check st);
     SI.CC.on_new_term (SI.cc si) (cc_on_new_term si st);
     SI.CC.on_pre_merge (SI.cc si) (cc_on_pre_merge si st);
     st
