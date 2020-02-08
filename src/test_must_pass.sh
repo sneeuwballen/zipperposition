7f1f0d40b903e1ed0f63412bbad5d09dfdb1f31e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-02-07 18:47:25 -0600

    test: fix "must pass" test

--- src/test_must_pass.sh
+++ src/test_must_pass.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ZIPPER=$1
-cat "../examples/must_pass.txt" | while read i ; do
-  echo "must pass: $i"; 
-  (${ZIPPER} $i -t 5 -o none | grep "SZS status Theorem" > /dev/null) \
-   || (echo "must-pass failed on $$i" && exit 1)
-done
+while read i ; do
+  echo "must pass: ../$i";
+  (${ZIPPER} "../${i}" -t 5 -o none | grep "SZS status Theorem" > /dev/null) \
+   || (echo "must-pass failed on ${i}" && exit 1)
+done < "../examples/must_pass.txt"
