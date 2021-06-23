218cd09e186548eb30a20b40c02b1c1d1e97f1a3
Jasmin Blanchette <jasmin.blanchette@gmail.com>
2021-06-23 17:17:49 +0200

    more SAT solver invocation for QLE

--- src/prover_calculi/qle.ml
+++ src/prover_calculi/qle.ml
@@ -17,43 +17,31 @@ module Make(E : Env.S) : S with module Env = E = struct
   module L = Literal
   module T = Term
   module SAT = Sat_solver.Make ()
-  module VarPayload = struct 
-    type t = ID.t * bool
-    let dummy = (ID.gensym (), false)
-  end
-  module BoolVar : Bool_lit_intf.S with type payload = VarPayload.t = struct
-    include Bool_lit.Make(struct 
-      include VarPayload
-    end)
-
-    let pp out bv =
-      CCFormat.fprintf out "%a%s" ID.pp (fst (payload bv))
-        (if snd (payload bv) then "+" else "-")
-  end
 
   let do_qle cs =
     (* TODO: Check first-orderness of problem. *)
-    let sym_of_lit lit =
+    let pred_of_lit lit =
       if L.is_predicate_lit lit then (
         match lit with
         | L.Equation(lhs, _, _) ->
           if (T.is_const (T.head_term lhs)) then (
             let sym = T.as_const_exn (T.head_term lhs) in
-            let pos_var = BoolVar.make (sym, true) in
-            let neg_var = BoolVar.make (sym, false) in
-            Some (sym, (pos_var, neg_var))
+            Some sym
           ) else None
         | _ -> None
       ) else None
     in
     let syms = ID.Tbl.create 128 in
     CS.iter (fun c ->
-      Array.iter (fun (pred, vars) -> ID.Tbl.replace syms pred vars)
-        (CCArray.filter_map sym_of_lit (C.lits c))) cs;
-    CCFormat.printf "%a@." (ID.Tbl.pp ID.pp (CCPair.pp BoolVar.pp BoolVar.pp))
+      Array.iter (fun pred ->
+          if not (ID.Tbl.mem syms pred) then
+            ID.Tbl.replace syms pred (BBox.make_fresh (), BBox.make_fresh ()))
+        (CCArray.filter_map pred_of_lit (C.lits c))) cs;
+    CCFormat.printf "%a@." (ID.Tbl.pp ID.pp (CCPair.pp SAT.Lit.pp SAT.Lit.pp))
       syms;
     Iter.iter (fun (pos_var, neg_var) ->
-        SAT.add_clause ~proof:Proof.Step.trivial [BoolVar.neg pos_var])
+        SAT.add_clause ~proof:Proof.Step.trivial
+          [SAT.Lit.neg pos_var; SAT.Lit.neg neg_var])
       (ID.Tbl.values syms)
   let setup () =
     Signal.once Env.on_start
