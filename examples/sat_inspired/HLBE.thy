86ed511f60753f6f1c702733d1799c91ef593399
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-04-05 13:56:09 +0200

    added examples

--- examples/sat_inspired/HLBE.thy
+++ examples/sat_inspired/HLBE.thy
@@ -13,13 +13,63 @@ lemma
   shows False
   oops
 
-(* FLE should simplify "blue \<or> yellow" to "yellow", but somehow this is handled
-   by UnitHLE? *)
+(* FLE should simplify "blue \<or> yellow" to "yellow". *)
 lemma
   assumes
     "blue \<longrightarrow> cyan"
     "blue \<longrightarrow> \<not> cyan"
     "blue \<or> yellow"
+  shows False
+  oops
+
+(* CongHLE+ should simplify "s = t \<or> u s a = u t a \<or> C" to "u s a = u t a \<or> C" *)
+lemma
+  assumes
+    "s = t \<or> u s a = u t a \<or> C"
+  shows False
+  oops
+
+(* CongHLE- should simplify "s \<noteq> t \<or> u s a \<noteq> u t a \<or> C" to "s \<noteq> t \<or> C" *)
+lemma
+  assumes
+    "s \<noteq> t \<or> u s a \<noteq> u t a \<or> C"
+  shows False
+  oops
+
+(* UnitHLE should simplify "q \<or> C \<or> D" to "C \<or> D" *)
+lemma
+  assumes
+    "\<forall>x. p x a"
+    "p b a \<longrightarrow> \<not> q"
+    "q \<or> C \<or> D"
+  shows False
+  oops
+
+(* HTR should simplify "green \<or> blue \<or> yellow \<or> red" to "green \<or> blue" *)
+lemma
+  assumes
+    "\<not> green \<longrightarrow> gray"
+    "gray \<longrightarrow> blue"
+    "green \<or> blue \<or> yellow \<or> red"
+  shows False
+  oops
+
+(* FLR should simplify "blue \<or> C \<or> D" to "blue" *)
+lemma
+  assumes
+    "cyan \<longrightarrow> blue"
+    "\<not> cyan \<longrightarrow> green"
+    "green \<longrightarrow> blue"
+    "blue \<or> C \<or> D"
+  shows False
+  oops
+
+(* UnitHTR should simplify "q \<or> C \<or> D" to "q" *)
+lemma
+  assumes
+    "\<forall>x. p x a"
+    "p b a \<longrightarrow> q"
+    "q \<or> C \<or> D"
   shows False
   sledgehammer [zipperposition, overlord, dont_slice] (assms)
   oops
