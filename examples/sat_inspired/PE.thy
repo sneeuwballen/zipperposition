145bf1a22d4d804c3c415a43f3ce8603b532410c
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-05-03 16:14:48 +0200

    more SAT-inspired elimination examples -- with Petar V.

--- examples/sat_inspired/PE.thy
+++ examples/sat_inspired/PE.thy
@@ -7,17 +7,53 @@ begin
 (* 1. SPE should eliminate p *)
 lemma
   assumes
-    "q a \<or> p x"
-    "p (f x) \<or> r b"
+    "u = v \<or> p x"
+    "p (f x) \<or> w = x"
+    "y = z"
   shows False
-  sledgehammer [zipperposition, overlord, dont_slice] (assms)
+  (* sledgehammer [zipperposition, overlord, dont_slice] (assms) *)
   oops
 
-(*
-\<not> p x \<or> q x
-\<not> p x \<or> r x \<or> s x
-p x \<or> \<not> q x \<or> \<not> r x
-p x \<or> \<not> q x \<or> \<not> s x
-*)
+(* 2. SPE should eliminate p *)
+lemma
+  assumes
+    "p one \<or> f one = two"
+    "p two \<or> f two = three"
+    "p three \<or> f three = four"
+    "\<not> p one \<or> f four = five"
+    "\<not> p two \<or> f five = six"
+  shows False
+  (* sledgehammer [zipperposition, overlord, dont_slice] (assms) *)
+  oops
+
+(* 3. SPE should eliminate p and q *)
+lemma
+  assumes
+    "p one \<or> f one = two"
+    "p two \<or> f two = three"
+    "p three \<or> f three = four"
+    "\<not> p one \<or> f four = five"
+    "\<not> p two \<or> f five = six"
+    "q one \<or> f one = two"
+    "q two \<or> f two = three"
+    "q three \<or> f three = four"
+    "\<not> q one \<or> f four = five"
+    "\<not> q two \<or> f five = six"
+  shows False
+  (* sledgehammer [zipperposition, overlord, dont_slice] (assms) *)
+  oops
+
+(* 4. SPE should not eliminate any predicate *)
+lemma
+  assumes
+    "p one \<or> \<not> p two"
+    "p two \<or> f two = three"
+    "p three \<or> f three = four"
+    "\<not> p one \<or> f four = five"
+    "\<not> p two \<or> f five = six"
+  shows False
+  (* sledgehammer [zipperposition, overlord, dont_slice] (assms) *)
+  oops
+
 
 end
