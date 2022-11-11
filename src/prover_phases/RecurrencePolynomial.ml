c134a59a5c4164272b92b174bd189cca5d0d8a37
Visa Nummelin <vvvalfa@gmail.com>
2022-11-11 16:22:52 +0100

    Ordering, coefficient view, and more module/object/typeclass stuff

--- src/prover_phases/RecurrencePolynomial.ml
+++ src/prover_phases/RecurrencePolynomial.ml
@@ -18,13 +18,16 @@ let (%>>) f g x y = g(f x y)
 let (<:>) r f = r:=f!r
 (* Lexicographic product of comparison functions onto tuples. *)
 let ( *** ) c1 c2 (x1,x2) (y1,y2) = match c1 x1 y1 with 0 -> c2 x2 y2 | r -> r
-(* Example: length_lex_list c = (fun l -> List.length l, l) %%> Stdlib.compare *** lex_list c *)
+(* Example: lex_list(-) [1;4;2] [1;3;3;3] > 0 where (-) compares positive ints without overflow. *)
 let rec lex_list c = curry(function
 | [], [] -> 0
 | [], _ -> 1
 | _, [] -> -1
 | x::xx, y::yy -> (c *** lex_list c) (x,xx) (y,yy))
+let length_lex_list c = (fun l -> length l, l) %%> (-) *** lex_list c
 let lex_array c = to_list %%> lex_list c
+let length_lex_array c = (fun l -> Array.length l, l) %%> (-) *** lex_array c
+
 let sum_list = fold_left (+) 0
 let sum_array = Array.fold_left (+) 0
 let index_of p l = fst(get_exn(find_idx p l))
@@ -95,33 +98,9 @@ module Poly(PD: PolyData) = struct
   [@@@warning "+8"]
 end
 
-(* Ordered lists representing sums g₁x₁+...+gₙxₙ. Algebraically the direct sum Gᕁ. *)
-module Sums(G: GROUP)(X: ORD) = struct
-  type t = (G.t * X.t) list
-  
-  let rec (++) p q = match p,q with
-  | p, [] -> p
-  | (c1,x1)::p1, (c2,x2)::p2 -> let ord = X.compare x1 x2 in
-    if ord = 0 then
-      let c = G.plus c1 c2 in
-      (if G.zero=c then p1 else (c,x1)::p1) ++ p2
-    else if ord > 0 then (c1,x1) :: p1++q
-    else q++p
-  | p, q -> q++p
-  
-  let (--) p q = p ++ map(map_fst G.inverse) q
-  
-  let zero = []
-  let plus = (++)
-  let inverse = (--)zero
-end
 
-(* module RecPoly(R: RING) = struct
-  module Base = Sums(R)(MonoOrder) (* RING ⊇ GROUP *)
-  include Base *)
-
-type 'f derop = D | O of 'f | X of 'f
 type var = int
+let compare_var = (-)
 type atom = I | V of var | T of Term.t
 type poly = op list list
 and op = C of Z.t | A of atom | D of var | XD of var | S of var | X of poly | O of (var*poly)list
@@ -165,19 +144,44 @@ let commut a b =
 | A _, b -> [[b;a]] (* Delicate: I want to simplify also _,A but only when A is fully right which must be taken into account somewhere at some point! *)
 | a, b -> [[a;b]]
 
+(* Equality for normalized polynomials. *)
+let rec poly_eq p = CCList.equal mono_eq p
+and mono_eq m n = m==n or CCList.equal(curry(function
+  | C a, C b -> Z.equal a b
+  | A(T t), A(T s) -> Term.equal t s
+  | X f, X g -> poly_eq f g
+  | O f, O g -> CCList.equal(CCPair.equal (=) poly_eq) f g
+  | a, b -> a = b
+)) m n
 
-(* Use through redefine_elimination_priority. *)
-(* let elimination_priority = ref ~= ~=[] *)
+(* Hash code for normalized polynomials. *)
+let rec poly_hash ?(depth=2) p = fold_left (fun h m -> 127*h + mono_hash ~depth m) 0 (take 4 p)
+and mono_hash ?(depth=2) = fold_left (fun h x -> 31*h + indet_hash ~depth x) 0
+and indet_hash ?(depth=2) = if depth=0 then ~=7 else function
+  | X f | O((_,f)::_) -> poly_hash ~depth:(depth-1) f
+  | A(T t) -> Term.hash t
+  | x -> Hashtbl.hash x
+
+let coef_view: poly -> (poly * op list) list =
+  map(take_drop_while(function C _ | X _ -> true | _-> false))
+  %> group_by ~eq:(snd%%>mono_eq) ~hash:(snd%>mono_hash)
+  %> map(fun coef_mono_list -> map fst coef_mono_list, snd(hd coef_mono_list))
+
+let lead_coef = fst % hd % coef_view
+let lead_main_ops = snd % hd % coef_view
+
 
 (* Append padding elements to the end of the shorter list l1 or l2 to make their lengths equal. *)
-let pad_by padding l1 l2 = (fun l -> l @ repeat ((length%%>max) l1 l2 - length l) [padding]) @@ (l1,l2)
+let pad_tail fill l1 l2 = (fun l -> l @ repeat ((length%%>max) l1 l2 - length l) [fill]) @@ (l1,l2)
+(* Append padding elements to the start of the shorter list l1 or l2 to make their lengths equal. *)
+let pad_head fill l1 l2 = (fun l -> repeat ((length%%>max) l1 l2 - length l) [fill] @ l) @@ (l1,l2)
 
-let rec trim_start delete = function o::l when delete o -> trim_start delete l | l->l
-let trim_end delete = rev % trim_start delete % rev
+let rec trim_head delete = function o::l when delete o -> trim_head delete l | l->l
+let trim_tail delete = rev % trim_head delete % rev
 
 (* Lexicographic comparison after 0-padding and reversal. E.g. [a] ≈ [a,0] ≤ [a,b] < [_,B] when 0≤b<B. *)
-let compare_later_dominant l1 l2 = uncurry(lex_list(-)) (rev @@ pad_by 0 l1 l2)
-(* TODO reverse all and use length-lex with appropriate guarantees *)
+let compare_later_dominant l1 l2 = uncurry(lex_list(-)) (rev @@ pad_tail 0 l1 l2)
+let compare_later_dominant = rev %%> length_lex_list(-)
 
 
 type 't ord = <compare: 't->'t->int>
@@ -210,20 +214,47 @@ let equal' k x y = 0=k#compare x y
 let min' k x y = if k#compare x y < 0 then x else y
 let max' k x y = if k#compare x y > 0 then x else y
 let sum' m = fold_left m#plus m#o
+let neg' g = g#minus g#o
+let is0 m = equal' m m#o
 
-(* From ordered group of {r₀,r₁,r₂,...} form ordered group of ordinals r₀+r₁ω+r₂ω²+r₃ω³+... represented by lists [r₀;r₁;r₂;...]. The results of these operations are never of the form [...;0]. *)
+module Monoid_of_object(M: sig type t val monoid: t monoid end) = struct
+  type t = M.t
+  let zero = M.monoid#o
+  let plus = M.monoid#plus
+end
+
+module Group_of_object(G: sig type t val group: t group end) = struct
+  include Monoid_of_object(struct
+    type t = G.t
+    let monoid = (G.group :> t monoid)
+  end)
+  let inverse = neg' G.group
+end
+
+module Ring_of_object(R: sig type t val ring: t ring end) = struct
+  module G = Group_of_object(struct
+    type t = R.t
+    let group = (R.ring :> t group)
+  end)
+  let times = R.ring#times
+  let of_Z = R.ring#of_Z
+end
+
+
+(* From ordered group of {r₀,r₁,r₂,...} form ordered group of pseudo-ordinals r₀+r₁ω+r₂ω²+...+rₙωⁿ represented by lists [r₀;...;rₙ]. Here ω is an infinitely large element. The results are never of the form [...;0] but inputs may be. *)
 let infinities r : 'r list ord_group =
-  let with_same_size f = pad_by r#o %>> uncurry f in
-  let map2_trim f = with_same_size(map2 f) %>> trim_end(equal' r r#o) in
+  (* let with_same_size f = pad_tail r#o %>> uncurry f in
+  let map2_trim f = with_same_size(map2 f) %>> trim_tail(equal' r r#o) in *)
+  let map2_trim f = pad_tail r#o %>> uncurry(map2 f) %>> trim_tail(is0 r) in
   object
-    method compare = with_same_size(rev %%> lex_list r#compare)
+    method compare = rev%trim_head(is0 r) %%> length_lex_list r#compare
     method plus = map2_trim r#plus
     method minus = map2_trim r#minus
     method o = []
   end
 
 
-let total_weight w weight = let rec recW _'=_'|>
+let total_weight w weight = let rec recW i=i|>
   fold_left(fun sum op -> w#plus sum (match op with
     | C _ | A I -> w#o
     | A(V i) -> weight(`V i)
@@ -237,21 +268,87 @@ let total_weight w weight = let rec recW _'=_'|>
 
 let total_deg = total_weight theZ ~=Z.one
 
+type simple_indeterminate = [`D of var |`O of var*poly |`S of var |`T of Term.t |`V of var]
 type indeterminate_shape = [`I|`C|`V|`O|`S|`D|`XD|`X|`T]
-let indeterminate_order: indeterminate_shape list ref = ref [`I;`C;`V;`O;`S;`D;`XD;`X]
+let indeterminate_order: indeterminate_shape list ref = ref [`I;`C;`V;`O;`S;`D;`XD;`X;`T]
 
 let indeterminate_shape = function C _->`C | S _->`S | D _->`D | XD _->`XD | O _->`O | X _->`X | A(V _)->`V | A I->`I | A(T _)->`T
 
-let tiebreaker = lex_list(fun x y ->
+let rec compare_mono_by weights = total_weight(infinities int_alg) weights %%> compare_later_dominant
+
+and compare_poly_by weights = lex_list(compare_mono_by weights)
+
+and tiebreak_by weights = lex_list(fun x y ->
   let xw,yw = flip index_of' !indeterminate_order % indeterminate_shape @@ (x,y) in
   if xw<yw then -1 else if xw>yw then 1 else match x,y with
-    | C x, C y -> Z.compare x y (* TODO sign-compare *)
+    | A I, A I -> 0
+    | C x, C y -> Z.((compare***compare) (abs x, x) (abs y, y))
     | D x, D y | S x, S y | XD x, XD y | A(V x), A(V y) -> compare x y
-    | _ -> raise TODO)
+    | X x, X y -> compare_poly_by weights x y
+    | O x, O y -> lex_list(compare_var *** compare_poly_by weights) x y
+    | A(T x), A(T y) -> Term.compare x y
+    | _ -> assert false (* If xw=yw, variant constructors of x,y must equal and they are covered above. *))
 
-let compare_mono_by weights m n = (map weights %%> compare_later_dominant) m n
+let indeterminate_weights: (simple_indeterminate -> int list) ref = ref~=[]
+let rev_cmp_mono = flip(compare_mono_by !indeterminate_weights)
 
-(* let compare_mono = (fun(c,m,s) -> !elimination_priority m s, (total_deg(c,m,s), (s, (m, c))))
-  %%> compare_later_dominant *** (-) *** compare_shifts *** lex_array(-) *** Z.compare
 
-let rev_cmp_mono = flip compare_mono *)
+(* Ordered lists representing sums g₁x₁+...+gₙxₙ. Algebraically the direct sum Gᕁ. *)
+module Sums(Parameters: sig
+  type g val group: g group
+  type x val ord: x ord
+end) = struct
+  open Parameters
+  (* type t = (g*x) list *)
+  (* It'd be an error to define the type above because the final "include" also defines it. Is there a more idiomatic way—as with signatures one can "include...with t:=t"? *)
+  
+  let rec (++) p q = match p,q with
+  | p, [] -> p
+  | (c1,x1)::p1, (c2,x2)::p2 -> let ord = ord#compare x1 x2 in
+    if ord = 0 then
+      let c = group#plus c1 c2 in
+      (if group#o=c then id else cons(c,x1)) (p1++p2)
+    else if ord > 0 then (c1,x1) :: p1++q
+    else q++p
+  | p, q -> q++p
+  
+  let (--) p q = p ++ map(map_fst(group#minus group#o)) q
+  
+  let group = object method o=[] method plus=(++) method minus=(--) end
+  include Group_of_object(struct
+    type t = (g*x) list
+    let group = group
+  end)
+end
+
+
+module RecPoly(Parameters: sig
+  type coef val ring: coef ring
+end) = struct
+  
+  open Parameters
+  type var = int
+  let compare_var = (-)
+  type atom = I | V of var | T of Term.t
+  type poly = op list list
+  and op = C of Z.t | A of atom | D of var | XD of var | S of var | X of poly | O of (var*poly)list
+
+  let eval_at ~var at = O[var, [[C at; A I]]]
+
+  include Sums(struct
+    type g = coef
+    let group = (ring :> g group)
+    type x = op list
+    let ord = raise TODO
+  end)
+
+  let ring: t ring = object
+    (* Note: classless “immediate” objects cannot be inherited like {record with ...}, see https://v2.ocaml.org/manual/objectexamples.html#s%3Aimmediate-objects *)
+    method o=group#o
+    method plus=group#plus
+    method minus=group#minus
+    
+    method of_Z z = [ring#of_Z z, [A I]]
+    method times = raise TODO
+  end
+end
