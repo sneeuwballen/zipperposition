699b3a75d0f71d684b2be20f31409a18050fa6fa
nartan <bozec.tanguy@gmail.com>
2025-12-10 14:28:07 +0100

    cleaning up files that don't belong

--- base_test.sh
+++
@@ -1,8 +0,0 @@
-
-while read p; do
-    timeout 45\
-        ./portfolio/portfolio.lams.parallel.py "../tptp_benchmarks/poly_problems/TF1/$p" 30 /tmp/ true | grep "status "
-    timeout 45\
-        ./zipperposition.exe --mode best --progress -o none --e-call-point 0.0 --try-e "./eprover-ho" -t 60 "../tptp_benchmarks/poly_problems/TF1/$p" | grep "status "
-    echo "done $p"
-done < "$1"
